b3e92ccad30b4ee7dfabf5163312b9b02c42009a
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-03-06 10:45:57 +0000

    player can enter name

--- app.rb
+++ app.rb
@@ -3,14 +3,20 @@ require_relative 'lib/player.rb'
 require_relative 'lib/game.rb'
 
 class RockPaperScissors < Sinatra::Base
-
+  attr_reader :player_1
   get '/' do
     erb :index
   end
 
   post '/names' do
-    player_1 = params[:player_name]
+    @player = Player.new(params[:player_name])
+    erb :choice
   end
+
+  # get '/choice' do
+  #   @player = player_1
+  #   erb :choice
+  # end
 
    run! if app_file == $0
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,1 +1,9 @@
 require './app'
+
+describe Player do
+  subject(:player) {Player.new('Nora')}
+
+  it "can return it's name" do
+    expect(player.name).to eq "Nora"
+  end
+end
