2dd4085ff3cb349a92bcd9dec26b43332f0c648d
Olivia Thetgyi <othetgyi@gmail.com>
2020-03-06 11:29:10 +0000

    RockPaper Scissors setup

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
-  get '/test' do
-    'test page'
+  get '/' do
+    'Welcome to Rock, Paper, Scissors!'
   end
    run! if app_file == $0
 end
