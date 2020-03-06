1288693d332cd0d40dac74b24810ee7e0cb3986a
Hodan <hodanahmed0000@gmail.com>
2020-03-06 11:41:54 +0000

    User Story 1: Created passing test where there is a field for user to enter name & submit button

--- spec/features/main_page_spec.rb
+++ spec/features/main_page_spec.rb
@@ -1,9 +1,12 @@
 require './app'
 
 feature 'main page' do
+
   scenario 'Can show main page content' do
     visit '/'
     save_and_open_page
-    expect(page).to have_content('Enter your name')
+    expect(page).to have_content('Enter name')
+    expect(page).to have_button('START!')
+    expect(page).to have_field('Your Name:')
   end
 end
