a9accc48a4f4a9d3f5ab0bdbd03658df02188ef7
Ziyao Zheng <ziyaozheng@Ziyaos-MacBook-Pro.local>
2020-03-06 12:46:32 +0000

    #enter_name_spec:shows the name tesst passed

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 # This is the controller of the RPS programme
 require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
-   run! if app_file == $0
-
-   get '/' do
+  run! if app_file == $0
+  #1 we need a home page for which contents are designed in index.erb
+  get '/' do
     erb :index
-   end 
+  end 
+
+  #2 After designed the form we want the user input post to the server 
+  #  so we can use it later to assign it to a variable etc.  
+  #3 Users should also be able to see their name with some greeting or guidance for next step, which designed in play.erb 
+  post '/name' do 
+    @player_name = params[:player_name]
+    erb :play
+  end 
 end
--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,5 +1,6 @@
 feature 'Allow user to enter name' do 
   scenario 'shows name on page' do 
+    visit('/')
     fill_in 'name', with: 'Pikachu'
     click_button "Let's play!"
     expect(page).to have_content 'Hello Pikachu!'
