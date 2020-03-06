3a854c270817e42a35b26af73a645e22b60ac3a9
Olivia Thetgyi <othetgyi@gmail.com>
2020-03-06 12:49:24 +0000

    Added test and feature to add a name

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
   get '/' do
-    'Welcome to Rock, Paper, Scissors!'
+    erb :index
+  end
+
+  post '/name' do
+    #pass the name the user has submitted to the instance variable
+    @player_name = params[:player_name]
+    response = erb :choose_weapon
+    response
   end
    run! if app_file == $0
 end
--- spec/features/test_page_spec.rb
+++ spec/features/test_page_spec.rb
@@ -1,8 +1,14 @@
 require './app'
 
-feature 'test page' do
-  scenario ('visit test') do
-    visit '/test'
-    expect(page).to have_content('test page')
-  end  
+feature 'homepage' do
+  scenario "I should be able to add a name" do
+    # visit the homepage
+    visit '/'
+    # fill in the name field form
+    fill_in 'name', with: 'Olivia'
+    # click on the submit button
+    click_button 'submit'
+    # See some content that tells us that it has taken the name in
+    expect(page).to have_content('Ready to play, Olivia? Choose your weapon!')
+  end
 end
