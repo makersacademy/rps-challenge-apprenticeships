cad62840f9e3cc55f2f2761714d551f19e8132f0
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-03-06 12:08:51 +0000

    feature works, tests don't

--- lib/game.rb
+++ lib/game.rb
@@ -5,13 +5,21 @@ class Game
   end
 
   def game_choice
-    @game_choice = ["Rock","Paper","Scissor"].sample
+    @game_choice = ["Rock","Paper","Scissors"].sample
   end
 
   def winner
     game_choice
-    if @game_choice == "Paper" && @player.choice == "Rock"
+    if @game_choice == @player.choice
+      @winner = "It's a tie"
+    elsif @game_choice == "Paper" && @player.choice == "Rock"     
       @winner = "#{@game_choice} beats #{@player.choice}. You lose!"
+    elsif @game_choice == "Rock" && @player.choice == "Scissors"
+      @winner = "#{@game_choice} beats #{@player.choice}. You lose!"
+    elsif @game_choice == "Scissors" && @player.choice == "Paper"
+      @winner = "#{@game_choice} beats #{@player.choice}. You lose!"
+    else
+      @winner = "#{@player.choice} beats #{@game_choice} . You win!"
     end
   end
 end
