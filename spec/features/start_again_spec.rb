4f525179b41bf61f62924a204151565244788cce
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-03-06 13:48:54 +0000

    failing test to start again

--- spec/features/start_again_spec.rb
+++ spec/features/start_again_spec.rb
@@ -1,1 +1,12 @@
 require './app'
+
+feature 'Start Again' do
+  scenario 'start again' do
+    visit('/')
+    fill_in :player_name, with: 'Nora'
+    click_button 'Submit'
+    click_button 'Rock'
+    click_button 'Start Again'
+    expect(page).to have_content 'Rock Paper Scissors!'
+  end
+end
