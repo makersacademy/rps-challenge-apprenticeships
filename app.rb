28bcd9a3cf8ef14cea43e55c5cdd5f65a93329d1
Student <student@makerss-air-2.home>
2020-03-06 15:03:26 +0000

    Pass:rspec Fail:manual

--- app.rb
+++ app.rb
@@ -1,15 +1,20 @@
 require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
   get '/' do
-   erb :index
+    erb :index
   end
-  post '/names' do
+  get '/test' do
+    'Testing infrastructure working!'
+  end
+  post '/name' do
     session[:name] = params[:name]
     redirect '/play'
   end
-  get '/play' do
+  get '/play' do 
+    @name = session[:name]
     erb :play
   end
-
-   run! if app_file == $0
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
--- spec/features/player_name_spec.rb
+++ spec/features/player_name_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
-feature 'Enter name!' do
+feature 'Enter names' do
   scenario 'submitting names' do
     visit('/')
-    fill_in with: 'Red'
-    click_button 'submit'
-    expect(page).to have_content 'Red'
+    fill_in :name, with: 'Dave'
+    click_button 'Submit'
+    expect(page).to have_content 'Dave'
   end
 end
