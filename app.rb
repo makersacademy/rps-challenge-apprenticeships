60045eb6dcd439cebc406498aeacb4c12eb98ceb
Ziyao Zheng <ziyaozheng@Ziyaos-MacBook-Pro.local>
2020-03-06 11:33:17 +0000

    created failed test for form submission

--- app.rb
+++ app.rb
@@ -1,8 +1,6 @@
+# This is the controller of the RPS programme
 require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
-  get '/test' do
-    'test page'
-  end
    run! if app_file == $0
 
    get '/' do
---
+++ spec/features/enter_name_spec.rb
@@ -0,0 +1,7 @@
+feature 'Allow user to enter name' do 
+  scenario 'shows name on page' do 
+    fill_in 'name', with: 'Pikachu'
+    click_button "Let's play!"
+    expect(page).to have_content 'Hello Pikachu!'
+  end 
+end 
