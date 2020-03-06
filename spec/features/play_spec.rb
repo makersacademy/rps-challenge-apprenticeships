50d104feaad7b4a9b931a12232b71217b1364faf
Student <student@makerss-air-2.home>
2020-03-06 16:15:57 +0000

    Fail:back_to_single_within_item

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -9,9 +9,9 @@ feature 'play game' do
   
 
   scenario 'see the items i can play' do
-    expect(page).to have_content 'Rock'
-    expect(page).to have_content 'Paper'
-    expect(page).to have_content 'Scissors'
+    expect(page).to have_button 'Rock'
+    expect(page).to have_button 'Paper'
+    expect(page).to have_button 'Scissors'
   end
 
   scenario 'choose a item' do
