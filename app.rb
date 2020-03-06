2191ae7016d0e74335c38caa0eb24bfb323abf65
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-03-06 10:23:32 +0000

    made file structure

--- app.rb
+++ app.rb
@@ -1,7 +1,16 @@
 require 'sinatra/base'
+require_relative 'lib/player.rb'
+require_relative 'lib/game.rb'
+
 class RockPaperScissors < Sinatra::Base
-  get '/test' do
-    'test page'
+
+  get '/' do
+    erb :index
   end
+
+  post '/names' do
+    player_1 = params[:player_name]
+  end
+
    run! if app_file == $0
 end
