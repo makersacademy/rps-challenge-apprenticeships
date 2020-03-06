c8fbeb950b41f6f2b68efe7dc78220ea384e8689
Olivia Thetgyi <othetgyi@gmail.com>
2020-03-06 13:11:08 +0000

    Wrote a failing test to check for ability to choose weapon

--- spec/features/test_page_spec.rb
+++ spec/features/test_page_spec.rb
@@ -11,4 +11,18 @@ feature 'homepage' do
     # See some content that tells us that it has taken the name in
     expect(page).to have_content('Ready to play, Olivia? Choose your weapon!')
   end
+
+  scenario "I should be able to choose a weapon" do
+    visit '/'
+    # fill in the name field form
+    fill_in 'name', with: 'Olivia'
+    # click on the submit button
+    click_button 'submit'
+    # fill in the name field form
+    choose 'Rock'
+    # click on the submit button
+    click_button 'submit'
+    # See some content that tells us that it has taken the name in
+    expect(page).to have_content('You have chosen Rock!')
+  end
 end
