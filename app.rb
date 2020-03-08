bcbd84dea3a924d801b76ec4f3b430cd8a193ce4
FunmiBADev <funmiba@hotmail.com>
2020-03-08 11:32:33 +0000

    /play page updeated to welcome player

--- app.rb
+++ app.rb
@@ -6,5 +6,16 @@ class RockPaperScissors < Sinatra::Base
     erb(:index)
   end
 
+   post '/name' do 
+  	session[:player] = params[:player]
+    redirect '/play'
+  end
+
+  get '/play' do 
+  	@player = session[:player]
+  	erb(:play)
+  end
+
+
    run! if app_file == $0
 end
--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -3,5 +3,7 @@ require './app'
 feature 'register name' do 
 	scenario 'submit name' do 
 		register_and_play
+		expect(page).to have_content "Hello Jimmy it is you vs. Computer"
+
 	end
 end
