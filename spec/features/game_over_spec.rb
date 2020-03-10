5170bd1dd92d60f8f98baf91395e5c41f8efa986
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-03-10 16:48:30 +0000

    used seed in test

--- spec/features/game_over_spec.rb
+++ spec/features/game_over_spec.rb
@@ -1,17 +1,35 @@
 require './app'
 
 feature 'Choose option' do
-  scenario 'choosing rock' do
+  scenario 'computer chooses scissors' do
+    srand 1234
     visit('/')
     fill_in :player_name, with: 'Nora'
     click_button 'Submit'
     click_button 'Rock'
-    if page.has_content?("Computer chose Scissors")
-      expect(page).to have_content "Rock beats Scissors. You win!"
-    elsif page.has_content?("Computer chose Rock")
-      expect(page).to have_content "It's a tie"
-    else
-      expect(page).to have_content "You lose"
-    end
+
+    expect(page).to have_content "Rock beats Scissors. You win!"
+
   end
+
+  scenario 'computer chooses Rock' do
+    srand 1238
+    visit('/')
+    fill_in :player_name, with: 'Nora'
+    click_button 'Submit'
+    click_button 'Rock'
+
+    expect(page).to have_content "It's a tie"
+  end
+
+  scenario 'computer chooses Paper' do
+    srand 1236
+    visit('/')
+    fill_in :player_name, with: 'Nora'
+    click_button 'Submit'
+    click_button 'Rock'
+
+    expect(page).to have_content "Paper beats Rock. You lose!"
+  end
+  
 end
