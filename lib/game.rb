0b9a7f9e2104e9f489e4a074922bd8f608b1410d
Joe <josephhull676@yahoo.co.uk>
2020-05-15 14:25:35 +0100

    Added #play to game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,22 @@
+require_relative 'player'
+
 class Game
-  
+  def play(choice)
+    computer_choice = ["rock", "paper", "scissors"].sample
+
+    if choice.downcase == computer_choice
+      "It's a draw!"
+    elsif choice.downcase == 'rock' && computer_choice == 'scissors'
+      "You win!"
+    elsif choice.downcase == 'paper' && computer_choice == 'rock'
+      "You win!"
+    elsif choice.downcase == 'scissors' && computer_choice == 'paper'
+      "You win!"
+    elsif choice.downcase == 'rock' && computer_choice == 'paper'
+      "You lose!"
+    elsif choice.downcase == 'paper' && computer_choice == 'scissors'
+      "You lose!"
+    elsif choice.downcase == 'scissors' && computer_choice == 'rock'
+      "You lose!"
+    end 
 end
