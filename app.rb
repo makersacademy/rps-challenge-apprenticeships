713eaa71eedaf943fa97b1ebc6c49439db8a8468
olliekennedy <ollie.kennedy1@gmail.com>
2020-10-30 11:25:23 +0000

    Player name submission and route to play page

--- app.rb
+++ app.rb
@@ -1,7 +1,23 @@
 require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
+
   get '/test' do
     'test page'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/name' do
+    session[:player_name] = params[:name_entry]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_name = session[:player_name]
+    erb :play
   end
 
   run! if app_file == $0
--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,6 +1,20 @@
 feature 'home page' do
   scenario 'show correct content' do
     visit '/'
-    expect(page).to have_content('Hello!')
+    expect(page).to have_content('Welcome to Rock, Paper, Scissors!')
   end
+  scenario 'enter name and click submit' do
+    visit '/'
+    fill_in :name_entry, with: 'Ollie'
+    click_button :Submit
+    expect(page).to have_content('Welcome, Ollie - what is your move?')
+  end
+  # scenario 'expect play route to have three buttons - rock paper and scissors' do
+  #   visit '/'
+  #   fill_in :name_entry, with: 'Ollie'
+  #   click_button :Submit
+  #   expect(page).to have_selector(:link_or_button, 'rock')
+  #   expect(page).to have_selector(:link_or_button, 'paper')
+  #   expect(page).to have_selector(:link_or_button, 'scissors')
+  # end
 end
