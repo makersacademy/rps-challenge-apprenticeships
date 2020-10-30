38c82f7869ba0a5416e1fb8dc7c97c6e2896d6dd
zoethomas <zoethomas1540@gmail.com>
2020-10-30 19:59:56 +0000

    Added HTML to index.erb and added test to ensure this content is displayed to user

--- spec/features/index_feature_spec.rb
+++ spec/features/index_feature_spec.rb
@@ -3,4 +3,12 @@ feature 'Webpage loading correctly' do
     visit '/'
     expect(page.status_code).to eq(200)
   end
+
+  scenario 'Displays all text and text fields as expected' do
+    visit '/'
+    expect(page).to have_content('Rock Paper Scissors')
+    expect(page).to have_content('Enter name below to start:')
+    expect(page).to have_field('name')
+    expect(page).to have_button('Submit')
+  end
 end
