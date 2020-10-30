8819d75e6ebc24266a9f0f8eff4c94dc7507af70
Lorna Roberts <lornaroberts99@gmail.com>
2020-10-30 14:50:52 +0000

    user able to choose attack

--- app.rb
+++ app.rb
@@ -1,15 +1,42 @@
 require 'sinatra/base'
+require './lib/Player'
 class RockPaperScissors < Sinatra::Base
+
+enable :sessions
 
       get "/" do
           erb(:index)
       end
 
-      post "/game" do
-        p "hello!"
-        @user_name = params[:user_name]
+      post '/names' do
+        $player_1 = Player.new(params[:user_name])
+        $player_2 = Player.new("Computer")
+        redirect '/game'
+      end
+
+      get "/game" do
+        @player_1_name = $player_1.name
+        @player_2_name = $player_2.name
+        session[:player_1_points] = 0
+        session[:player_2_points] = 0
+        @player_1_points = session[:player_1_points]
+        @player_2_points = session[:player_2_points]
+        @player_1_choice = $player_1c
+        p @player_1_choice
+
         erb(:game)
       end
+
+
+
+
+      post "/choice" do
+       $player_1c = params[:user_choice]
+       p $player_1c
+      redirect '/game'
+      end
+
+
 
 
   run! if app_file == $0
