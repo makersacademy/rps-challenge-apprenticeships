445fb0bf5b5a8d771b631bb0b199f21ede312165
Joseph <jpkershaw17@gmail.com>
2020-10-30 15:09:49 +0000

    More feature test coverage

--- spec/features/test_page_spec.rb
+++ spec/features/test_page_spec.rb
@@ -20,29 +20,67 @@ feature 'Player1 page' do
     sign_in_and_play
     expect(page).to have_content "Joe choose your weapon!"
   end
-  scenario 'Ask the first player for their choice' do
+  scenario 'expects the three buttons as options' do
     visit '/'
     sign_in_and_play
     expect(page).to have_button('Rock')
     expect(page).to have_button('Paper')
     expect(page).to have_button('Scissors')
   end
-  scenario 'Ask the first player for their choice' do
+  scenario 'clicking Rock takes you to the next page' do
     visit '/'
     sign_in_and_play
     click_on "Rock"
     expect(page.current_path).to eq '/Player2'
   end
-  scenario 'Ask the first player for their choice' do
+  scenario 'clicking Paper takes you to the next page' do
     visit '/'
     sign_in_and_play
     click_on "Paper"
     expect(page.current_path).to eq '/Player2'
   end
-  scenario 'Ask the first player for their choice' do
+  scenario 'clicking Scissors takes you to the next page' do
     visit '/'
     sign_in_and_play
     click_on "Scissors"
     expect(page.current_path).to eq '/Player2'
+  end
+end
+
+feature 'Player2 page' do
+  scenario 'Ask player2 for their choice' do
+    visit '/'
+    sign_in_and_play
+    click_on "Paper"
+    expect(page).to have_content "Abby choose your weapon!"
+  end
+  scenario 'expects the three buttons as options' do
+    visit '/'
+    sign_in_and_play
+    click_on "Paper"
+    expect(page).to have_button('Rock')
+    expect(page).to have_button('Paper')
+    expect(page).to have_button('Scissors')
+  end
+  scenario 'clicking Rock takes you to the next page' do
+    visit '/'
+    sign_in_and_play
+    click_on "Paper"
+    click_on "Rock"
+    expect(page.current_path).to eq '/choices'
+  end
+  scenario 'clicking Paper takes you to the next page' do
+    visit '/'
+    sign_in_and_play
+    click_on "Paper"
+    click_on "Paper"
+    expect(page.current_path).to eq '/choices'
+  end
+  scenario 'clicking Scissors takes you to the next page' do
+    visit '/'
+    sign_in_and_play
+    click_on "Paper"
+    click_on "Scissors"
+    expect(page.current_path).to eq '/choices'
   end
 end
