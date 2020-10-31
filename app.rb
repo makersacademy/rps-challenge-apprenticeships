5f257f9ddd57df303015364a6b276394bbfc540d
Lorna Roberts <lornaroberts99@gmail.com>
2020-10-31 17:24:00 +0000

    done minus score and imgs

--- app.rb
+++ app.rb
@@ -15,23 +15,35 @@ enable :sessions
       end
 
       get "/game" do
-        @player_1_name = $player_1.name
-        @player_2_name = $player_2.name
-        session[:player_1_points] = 0
-        session[:player_2_points] = 0
         @player_1_points = session[:player_1_points]
         @player_2_points = session[:player_2_points]
+        session[:player_1_points] = 0
+        session[:player_2_points] = 0
+        @player_1_name = $player_1.name
+        @player_2_name = $player_2.name
         @player_1_choice = $player_1c
         if @player_1_choice
         @player_2_choice =["rock", "paper", "scissors"].sample
-end
+      end
         if (@player_1_choice == "rock" && @player_2_choice == "rock" ||
           @player_1_choice == "paper" && @player_2_choice == "paper" ||
           @player_1_choice == "scissors" && @player_2_choice == "scissors")
           @result = "Draw!"
+        elsif (@player_1_choice == "rock" && @player_2_choice == "scissors" ||
+          @player_1_choice == "paper" && @player_2_choice == "rock" ||
+          @player_1_choice == "scissors" && @player_2_choice == "paper")
+          @result = @player_1_name + " wins!"
+          @player_1_points += 1
+        elsif (@player_2_choice == "rock" && @player_1_choice == "scissors" ||
+          @player_2_choice == "paper" && @player_1_choice == "rock" ||
+          @player_2_choice == "scissors" && @player_1_choice == "paper")
+          @result = @player_2_name + " wins!"
+          @player_2_points += 1
   end
+
         erb(:game)
-      end
+
+    end
 
 
 
