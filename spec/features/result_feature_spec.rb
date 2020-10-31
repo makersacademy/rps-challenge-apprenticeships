914760efcf8896f72be355e840cb683b2b12d48e
zoethomas <zoethomas1540@gmail.com>
2020-10-31 11:52:45 +0000

    Added code & tests for play again button

--- spec/features/result_feature_spec.rb
+++ spec/features/result_feature_spec.rb
@@ -23,3 +23,22 @@ feature 'Displays users selection correctly to the screen' do
     expect(page).to have_content('Bob chose: scissors')
   end
 end
+
+feature 'User can play again' do
+  scenario 'Play again button displayed' do
+    visit '/'
+    fill_in "name", with: 'Bob'
+    click_button 'Submit'
+    click_button 'Scissors'
+    expect(page).to have_button("Play Again")
+  end
+
+  scenario 'Play again button redirects back to play page' do
+    visit '/'
+    fill_in "name", with: 'Bob'
+    click_button 'Submit'
+    click_button 'Scissors'
+    click_button 'Play Again'
+    expect(page).to have_content('Click button to take turn')
+  end
+end
