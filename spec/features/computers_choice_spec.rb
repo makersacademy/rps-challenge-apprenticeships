7686b0db347e08ad750f0292f993530f74b939a3
Ben Jones <benwjones76@outlook.com>
2020-10-31 13:04:55 +0000

    fixed the tests that were looking got a specific string

--- spec/features/computers_choice_spec.rb
+++ spec/features/computers_choice_spec.rb
@@ -2,15 +2,15 @@ feature 'allow the computer player to pick a random attack' do
   scenario 'computer picks rock,paper or scissors' do
     sign_in_and_play()
     click_button('Paper')
-    expect(page).to have_content "The computer picked:"
+    expect(page).to have_content "Computer picked:"
   end
 
-  scenario 'the computer picked scissors' do
+  scenario 'Computer picked scissors' do
     sign_in_and_play()
     game = double(:game)
     srand(4)
     allow(game).to receive(:computer_attack)
     click_button('Paper')
-    expect(page).to have_content "The computer picked: Scissors"
+    expect(page).to have_content "Computer picked: Scissors"
   end
 end
--- spec/features/player_choice_spec.rb
+++ spec/features/player_choice_spec.rb
@@ -2,18 +2,18 @@ feature 'Allow player to pick their attack type' do
   scenario 'show player 3 options and display the one they picked(Rock)' do
     sign_in_and_play()
     click_button('Rock')
-    expect(page).to have_content 'Ben picked Rock'
+    expect(page).to have_content 'Ben picked: Rock'
   end
 
   scenario 'show player 3 options and display the one they picked(Paper)' do
     sign_in_and_play()
     click_button('Paper')
-    expect(page).to have_content 'Ben picked Paper'
+    expect(page).to have_content 'Ben picked: Paper'
   end
 
   scenario 'show player 3 options and display the one they picked(Scissors)' do
     sign_in_and_play()
     click_button('Scissors')
-    expect(page).to have_content 'Ben picked Scissors'
+    expect(page).to have_content 'Ben picked: Scissors'
   end
 end
