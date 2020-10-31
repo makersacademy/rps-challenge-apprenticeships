f22ae77ad2841bae8199ef5db820cb2ec4a4a42f
Fabiha <fabiha.hassan96@gmail.com>
2020-10-31 01:36:16 +0000

    Enter name and choose option

--- app.rb
+++ app.rb
@@ -1,8 +1,34 @@
 require 'sinatra/base'
+
 class RockPaperScissors < Sinatra::Base
   get '/test' do
     'test page'
   end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/name' do
+    $player_name = params[:player_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_name = $player_name
+    erb :play
+  end
+
+  post '/throw' do
+    $throw = params[:throw]
+    redirect '/result'
+  end
+
+  get '/result' do
+    @throw = $throw
+    erb :result
+  end
+
 
   run! if app_file == $0
 end
