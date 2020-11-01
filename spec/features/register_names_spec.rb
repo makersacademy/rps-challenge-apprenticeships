2e9f9e097d99dea734bd89389531c2df5b2bba98
deborah adebiyi <deborahadebiyi@hotmail.co.uk>
2020-11-01 17:18:57 +0000

    Move buttons added

--- spec/features/register_names_spec.rb
+++ spec/features/register_names_spec.rb
@@ -7,3 +7,15 @@ feature 'Register names' do
     expect(page).to have_content 'Jake the marketeer'
   end
 end
+
+feature 'Playing a game of RPS' do
+  scenario 'Move choices are displayed as buttons' do
+  visit('/play')
+  click_button 'Rock'
+  click_button 'Paper'
+  click_button 'Scissors'
+  expect(page).to have_content 'Rock'
+  expect(page).to have_content 'Paper'
+  expect(page).to have_content 'Scissors'
+  end
+end
