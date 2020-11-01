0690297cc1fa38457bffacb68aeb8e91c10e0305
deborah adebiyi <deborahadebiyi@hotmail.co.uk>
2020-11-01 17:56:23 +0000

    Player choosig move

--- app.rb
+++ app.rb
@@ -12,11 +12,17 @@ class RockPaperScissors < Sinatra::Base
 
   get '/play' do
     @player_1_name = session[:player_1_name]
+    @move = session[:move]
     erb :play
   end
 
   post '/register' do
     session[:player_1_name] = params[:player_1_name]
+    redirect '/play'
+  end
+
+  post '/play' do
+    session[:move] = params[:move]
     redirect '/play'
   end
 
--- spec/features/register_names_spec.rb
+++ spec/features/register_names_spec.rb
@@ -11,11 +11,16 @@ end
 feature 'Playing a game of RPS' do
   scenario 'Move choices are displayed as buttons' do
   visit('/play')
+  expect(page).to have_button 'Rock'
+  expect(page).to have_button 'Paper'
+  expect(page).to have_button 'Scissors'
+  end
+end
+
+feature 'Player choosing their move' do
+  scenario 'chooses Rock' do
+  visit('/play')
   click_button 'Rock'
-  click_button 'Paper'
-  click_button 'Scissors'
-  expect(page).to have_content 'Rock'
-  expect(page).to have_content 'Paper'
-  expect(page).to have_content 'Scissors'
+  expect(page).to have_content 'You have chosen Rock'
   end
 end
