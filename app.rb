e570d4cfbd52c90aa40d599615643e0226684d57
Eunice Kontor <Eunice.Kontor@ft.com>
2020-11-02 16:56:28 +0000

    present random choice for computer and announce winner

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/game'
 
 class RockPaperScissors < Sinatra::Base
   enable :sessions
@@ -13,21 +14,31 @@ class RockPaperScissors < Sinatra::Base
 
   post '/select_move' do
     session[:player] = params[:player]    
+    redirect '/select_move'
+  end
+
+  get '/select_move' do
+    @player = session[:player]
+    erb :play
+  end
+  
+  post '/play' do
+    game = Game.new
+    session[:choice] = params[:choice]
+    session[:computer_choice] = game.random_choice
     redirect '/play'
   end
 
   get '/play' do
+    game = Game.new
     @player = session[:player]
     @choice= session[:choice]
     @computer_choice = session[:computer_choice]
-    erb :play
+    @winner = game.winner(@choice, @computer_choice)
+    erb :result
   end
 
-  post '/play' do
-    session[:choice] = params[:choice]
-    session[:computer_choice] = :Rock    
-    redirect '/play'
-  end
+
 
   run! if app_file == $0
 end
