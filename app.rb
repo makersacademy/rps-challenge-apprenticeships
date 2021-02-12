96cead1a28751fd880675aca7df06695bc2e3588
Graemehedges <graeme.hedges@gmail.com>
2021-02-12 08:55:34 +0000

    refactored taking name to POST redirect GET

--- app.rb
+++ app.rb
@@ -1,14 +1,25 @@
 require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
-  post '/game' do
-    @name = params[:name]
+  post '/name' do
+    session[:name] = params[:name]
+    redirect '/game'
+  end
+
+  get '/game' do
+    @name = session[:name]
     erb :game
   end
 
   run! if app_file == $0
 end
+
+# EZ DEBUGGER :D
+# p '*' * 10
+# p
+# p '*' * 10
