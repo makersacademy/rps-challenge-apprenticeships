eccac8057158e24aa8432d5ca82d62e76517cd43
Vincenzo Florio <vincenzoflorio31@gmail.com>
2021-02-14 12:46:20 +0000

    Add public folder with stylesheet and instructions graphic

--- spec/features/index_feature_spec.rb
+++ spec/features/index_feature_spec.rb
@@ -4,19 +4,19 @@ feature 'Front page with display of options available' do
     expect(page).to have_content('ROCK PAPER SCISSORS!')
   end
 
-  scenario 'displays Rules option' do
+  scenario 'displays rules option' do
     visit('/')
     expect(page).to have_button('1. Rules')
   end
 
-  scenario 'visit the homepage' do
+  scenario 'display player vs computer option' do
     visit('/')
     expect(page).to have_button('2. Player vs Computer')
   end
 
-  scenario 'visit the homepage' do
+  scenario 'display player vs player option' do
     visit('/')
-    expect(page).to have_content('3. Player vs Player')
+    expect(page).to have_button('3. Player vs Player')
   end
 end
 
--- spec/features/instructions_feature_spec.rb
+++ spec/features/instructions_feature_spec.rb
@@ -1,7 +1,8 @@
 feature 'Instructions' do
   scenario 'display instructions image' do
     visit '/'
-    click_button('1. Instructions')
-    expect(page).to have_xpath('RPSLS.png')
+    click_button('1. Rules')
+    save_and_open_page
+    expect(page).to have_xpath("//img[@src='RPSLS.png']")
   end
 end
