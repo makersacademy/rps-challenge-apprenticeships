e6d73a7e648653f0645cd3531587ab575917e7cd
AlistairTait12 <alistair.tait1@gmail.com>
2021-02-15 12:38:42 +0000

    Made new weapon class which needs testing

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
 require 'sinatra/base'
-require_relative './lib/rock.rb'
-require_relative './lib/paper.rb'
-require_relative './lib/scissors.rb'
-require_relative './lib/lizard.rb'
-require_relative './lib/spock.rb'
-require_relative './lib/player_choice.rb'
-require_relative './lib/random_choice.rb'
+# require_relative './lib/rock.rb'
+# require_relative './lib/paper.rb'
+# require_relative './lib/scissors.rb'
+# require_relative './lib/lizard.rb'
+# require_relative './lib/spock.rb'
+# require_relative './lib/player_choice.rb'
+# require_relative './lib/random_choice.rb'
+
+require_relative './lib/weapon.rb'
 
 class RockPaperScissors < Sinatra::Base
   get '/test' do
@@ -27,11 +29,12 @@ class RockPaperScissors < Sinatra::Base
   end
 
   post '/outcome' do
-    @player_choice = PlayerChoice.new(params[:choice]).choice
-    @computer_choice = RandomChoice.new.choice
 
-    @player_score = @player_choice.win_status(@computer_choice.value)
-    @computer_score = @computer_choice.win_status(@player_choice.value)
+    @player_choice = Weapon.new(params[:choice])
+    @computer_choice = Weapon.new
+
+    @player_score = @player_choice.win_status(@computer_choice.choice)
+    @computer_score = @computer_choice.win_status(@player_choice.choice)
 
     erb(:outcome)
   end
