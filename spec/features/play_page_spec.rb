096cec988ce615d66b366f8d47247a5376647752
HarryCarton <harrycarton@hotmail.com>
2021-02-15 10:00:37 +0000

    added play again button

--- spec/features/play_page_spec.rb
+++ spec/features/play_page_spec.rb
@@ -13,8 +13,16 @@ feature 'play page' do
     choose 'rock'
     click_button 'Submit'
     expect(page).to have_content('Meg chose rock')
-    expect(page.text).to match(/Meg chose (rock|paper|scissors)/)
+    expect(page.text).to match(/Super Computer chose (rock|paper|scissors)/)
+    expect(page.text).to match(/(Meg wins|It's a draw|Super Computer wins)/)
   end
 
+  scenario 'check for replay button' do
+    sign_in_and_submit
+    click_link 'Play'
+    choose 'rock'
+    click_button 'Submit'
+    expect(page).to have_link('Play again')
+  end
 
 end
