bb1d8e78f7f73b87ab344903ed4ea369949c8e03
Alice Lui <aytlui@gmail.com>
2021-03-26 10:57:04 +0000

    created session

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
+
   get '/test' do
     'test page'
   end
@@ -9,9 +11,15 @@ class RockPaperScissors < Sinatra::Base
   end
 
   post '/players' do
-    @player_one_name = params[:player_one_name]
-    erb :players
+    session[:player_one_name] = params[:player_one_name]
+    redirect '/play'
   end
+
+  get '/play' do 
+    @player_one_name = session[:player_one_name]
+    erb :play
+  end
+
 
   run! if app_file == $0
 end
