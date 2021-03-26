0c126812b557a0a98820649404e9e0a8d2932b2c
Renaé St Louis <renaestlouis@Renaes-MacBook-Pro.local>
2021-03-26 14:10:34 +0000

    RPS Challenge

--- app.rb
+++ app.rb
@@ -1,8 +1,35 @@
 require 'sinatra/base'
+#require 'sinatra/reloader'
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
+
+  get "/" do
+    erb :index
+  end
+
+  post "/name" do
+    session[:name] = params[:name]
+    redirect '/game'
+  end
+
+  get "/game" do
+    @name = session[:name]
+    erb :game
+  end
+
+  post "/play" do
+    redirect '/winner'
+  end
+
+  get "/winner" do
+    @name = session[:name]
+    @computer = ["Rock", "Paper", "Scissors"].sample
+  end
+
+
+  #post "/winner" do - this page displays winners name in lights
   get '/test' do
     'test page'
   end
-
   run! if app_file == $0
 end
---
+++ spec/features/rock_paper_scissors_spec.rb
@@ -0,0 +1,9 @@
+require 'capybara'
+feature 'Playing Rock Paper Scissors' do
+    scenario 'choosing a play' do
+      visit('/play')
+      #choose a button from a dropdown
+
+      expect #to be returned a value from an array/a specific value
+    end
+end
