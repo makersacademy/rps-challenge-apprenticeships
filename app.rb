06b15cdf44918a4abaab4579715ab6b4d73bdde3
Alice Lui <aytlui@gmail.com>
2021-03-26 14:56:50 +0000

    added global variables

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 require 'sinatra/base'
 require './lib/random_selection'
 require './lib/game'
+require './lib/player'
 
 class RockPaperScissors < Sinatra::Base
-  enable :sessions
 
   get '/test' do
     'test page'
@@ -14,19 +14,21 @@ class RockPaperScissors < Sinatra::Base
   end
 
   post '/players' do
-    session[:player_one_name] = params[:player_one_name]
+    $player_one = Player.new(params[:player_one_name])
     redirect '/play'
   end
 
   get '/play' do 
-    @player_one_name = session[:player_one_name]
+    @player_one_name = $player_one.name
     erb :play
   end
 
   get '/result' do
-    @player_one_name = session[:player_one_name]
-    @p1_selection = params[:p1_selection]
-    @cpu_selection = RandomSelection.new.rps
+    @player_one_name = $player_one.name
+    @p1_selection = $player_one.picks(params[:p1_selection])
+    $cpu = Player.new("CPU")
+    @cpu_name = $cpu.name
+    @cpu_selection = $cpu.picks(RandomSelection.new.rps)
     @game = Game.new(@p1_selection, @cpu_selection)
     @message = @game.result
     erb :result
