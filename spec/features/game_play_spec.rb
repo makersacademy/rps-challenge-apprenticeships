23850c24c115132980a327708a7a0c9371dcb15e
rahat <rahat.ahmed1@outlook.com>
2021-03-29 10:24:23 +0100

    User Story 3, Computer Pick Randomly

--- spec/features/game_play_spec.rb
+++ spec/features/game_play_spec.rb
@@ -13,8 +13,13 @@ feature 'game play' do
     expect(page).to have_button 'Scissors'
   end
 
-  scenario 'player choose an option' do
+  scenario 'player picks an option' do
     click_button 'Rock'
     expect(page).to have_content "Your Weapon Is Rock!"
   end
+  
+  #scenario 'computer picks an option' do
+    #expect(page).to have_content "Computer Chose Rock!"
+  #end
+
 end
