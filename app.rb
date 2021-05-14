411fd00c8e14f13b7f097c809285113933cab4b2
Luke Czoboriek <LC169782@protonmail.com>
2021-05-14 10:18:36 +0100

    Run into an error where the names are not being produced

--- app.rb
+++ app.rb
@@ -8,12 +8,17 @@ class RockPaperScissors < Sinatra::Base
     erb :index
   end
 
-  post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
 
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
 
   run! if app_file == $0
 end
