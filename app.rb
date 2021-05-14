c7862ad2fe20687c2d30f849836b62b0b6a63a29
Luke Czoboriek <LC169782@protonmail.com>
2021-05-14 10:06:57 +0100

    Added first test
    Added my first feature test, and also made sure to add the index page to have inputs, decided to start off with 2 players for the multiplayer to be fullfilled

--- app.rb
+++ app.rb
@@ -4,5 +4,16 @@ class RockPaperScissors < Sinatra::Base
     'test page'
   end
 
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
+
+
   run! if app_file == $0
 end
