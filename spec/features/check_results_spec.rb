df9cc3f7f52e37c5bc853212ede6fc11b7cb7d08
Luke Czoboriek <LC169782@protonmail.com>
2021-05-14 18:11:24 +0100

    Added formatting

--- spec/features/check_results_spec.rb
+++ spec/features/check_results_spec.rb
@@ -3,20 +3,20 @@ feature 'Checks to see if the winner is correct' do
     sign_in_play
     srand(1)
     click_button 'Rock'
-    expect(page).to have_content 'And the winner is Luke'
+    expect(page).to have_content 'Congratulations, Luke'
   end
 
   scenario 'player chooses paper and wins' do
     sign_in_play
     srand(3)
     click_button 'Paper'
-    expect(page).to have_content 'And the winner is Luke'
+    expect(page).to have_content 'Congratulations, Luke'
   end
 
   scenario 'player chooses scissors and wins' do
     sign_in_play
     srand(2)
     click_button 'Scissors'
-    expect(page).to have_content 'And the winner is Luke'
+    expect(page).to have_content 'Congratulations, Luke'
   end
 end
