67094d40372f23cd692f0005e87b2734876f842e
Jay Mullen <81382978+Jaikido007@users.noreply.github.com>
2021-05-14 11:51:21 +0100

    As a marketeer. So that I can see my name in lights. I would like to register my name before playing an online game.

--- spec/features/test_page_spec.rb
+++ spec/features/test_page_spec.rb
@@ -1,6 +1,16 @@
 feature 'test page' do
   scenario 'visit test page' do
     visit '/test'
-    expect(page).to have_content('test page')
+    expect(page).to have_content('Test page')
+  end
+end
+
+# As a marketeer
+# So that I can see my name in lights
+# I would like to register my name before playing an online game
+feature 'register name' do
+  scenario 'player submits name and views on screen' do
+    register_and_play
+    expect(page).to have_content 'Jay'
   end
 end
