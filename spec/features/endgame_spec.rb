32b281f24fb7b1e73ce695bded98806828762f09
Adam R <adamroyal12@gmail.com>
2021-05-17 12:52:52 +0100

    added endgame spec

---
+++ spec/features/endgame_spec.rb
@@ -0,0 +1,33 @@
+feature 'endgame page' do
+  before do
+    sign_in
+  end
+
+  feature 'it tells you the computer choice' do
+    scenario 'computer chooses scissors' do
+    allow_any_instance_of(Game).to receive(:computer_move).and_return('Scissors')
+    click_button "Rock"
+    expect(page).to have_content "The computer chose Scissors."
+    end
+  end
+
+  feature 'it tells you the outcome of the game' do
+    scenario 'player win' do
+      allow_any_instance_of(Game).to receive(:computer_move).and_return('Scissors')
+      click_button "Rock"
+      expect(page).to have_content "You have bested fate. Leave with your life while the gods allow it."
+    end
+
+    scenario 'computer win' do
+      allow_any_instance_of(Game).to receive(:computer_move).and_return('Paper')
+      click_button "Rock"
+      expect(page).to have_content "You chose poorly. You are sentenced to rot in the outlands."
+    end
+
+    scenario 'neither win' do
+      allow_any_instance_of(Game).to receive(:computer_move).and_return('Paper')
+      click_button "Paper"
+      expect(page).to have_content "You draw! Do you dare tempt fate again?"
+    end
+  end
+end
