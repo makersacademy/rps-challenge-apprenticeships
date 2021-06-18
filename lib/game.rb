29dfd807d7fb74ebff50419ac18ba74164c952de
Arran Oakes <arran.oakes@gmail.com>
2021-06-18 12:46:32 +0100

    Game sets player 1 choice

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,13 @@
 class Game
-  attr_reader :player_1, :player_2
+  attr_reader :player_1, :player_2, :player_1_choice
 
   def initialize(player_1, player_2=Computer.new)
     @player_1 = player_1
     @player_2 = player_2
+  end
+
+  def set_player_1_choice(choice)
+    @player_1_choice = choice
   end
 
 end
--- spec/units/game_spec.rb
+++ spec/units/game_spec.rb
@@ -4,7 +4,6 @@ describe Game do
 subject(:object) { described_class.new(player_1) }
 let(:player_1) { double(:player_1)}
 let(:computer) { class_double(Computer) }
-
 # let(:pve_game) { Game.new(player_1, computer) }
 
   it "starts a game" do
@@ -24,9 +23,28 @@ let(:computer) { class_double(Computer) }
 
       it 'allows the computer to make a choice' do
         srand(4)
-        expect(subject.player_2.make_move).to eq "Scissors"
+        expect(object.player_2.make_move).to eq "Scissors"
       end
+
+
 
   end
 
+  context "when playing any game" do
+
+    describe '#set_player_1_choice' do
+      it 'can set player 1 choice as scissors' do
+        object.set_player_1_choice("scissors")
+        expect(object.player_1_choice).to eq("scissors")
+      end
+    end
+
+    describe '#calculate_winner' do
+
+      it "calculates rock beats paper" do
+
+      end
+
+    end
+  end
 end
