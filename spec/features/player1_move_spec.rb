4a7bc2d3004c0d4ca36223e7e867b3cee770f73a
anitaRead <areadman91@gmail.com>
2021-06-18 14:46:22 +0100

    Update player one feature tests

--- spec/features/player1_move_spec.rb
+++ spec/features/player1_move_spec.rb
@@ -1,20 +1,20 @@
-# feature 'play rock paper scissors' do
-#     scenario 'play rock' do
-#       sign_in_and_play
-#       choose(option: 'rock')
-#       click_button('submit')
-#       expect(page).to have_content 'Anita played: rock'
-#     end
-#     scenario 'play paper' do
-#         sign_in_and_play
-#         choose(option: 'paper')
-#         click_button('submit')
-#         expect(page).to have_content 'Anita played: paper'
-#     end
-#     scenario 'play scissors' do
-#         sign_in_and_play
-#         choose(option: 'scissors')
-#         click_button('submit')
-#         expect(page).to have_content 'Anita played: scissors'
-#     end
-# end
+feature 'play rock paper scissors' do
+    scenario 'play rock' do
+      sign_in_and_play
+      choose_rock
+      choose_scissors
+      expect(page).to have_content 'Anita played: rock'
+    end
+    scenario 'play paper' do
+        sign_in_and_play
+        choose_paper
+        choose_rock
+        expect(page).to have_content 'Anita played: paper'
+    end
+    scenario 'play scissors' do
+        sign_in_and_play
+        choose_scissors
+        choose_rock
+        expect(page).to have_content 'Anita played: scissors'
+    end
+end
