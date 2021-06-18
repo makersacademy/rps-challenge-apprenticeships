a95b44278c8a0c49fa693a61a8ddb9a6b1ad27d8
anitaRead <areadman91@gmail.com>
2021-06-18 11:56:03 +0100

    Add options to play paper or scissors

--- spec/features/play_rps_spec.rb
+++ spec/features/play_rps_spec.rb
@@ -7,4 +7,20 @@ feature 'play rock paper scissors' do
       click_button('submit')
       expect(page).to have_content 'Anita played: rock'
     end
+    scenario 'play paper' do
+        visit '/'
+        fill_in('player', with: 'Anita')
+        click_button('submit')
+        choose(option: 'paper')
+        click_button('submit')
+        expect(page).to have_content 'Anita played: paper'
+    end
+    scenario 'play scissors' do
+        visit '/'
+        fill_in('player', with: 'Anita')
+        click_button('submit')
+        choose(option: 'scissors')
+        click_button('submit')
+        expect(page).to have_content 'Anita played: scissors'
+    end
 end
