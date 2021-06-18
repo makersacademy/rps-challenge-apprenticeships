4d0b51e1b64ddb6a41a8be6e22ebb896ffddd416
Goncalo Gordo <goncalogordo@gmail.com>
2021-06-18 11:31:07 +0100

    add web_helpers to rspec tests

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -5,62 +5,48 @@
 feature 'play game rock-paper-scissors' do
 
   scenario 'player is presented the choices (rock, paper and scissors)' do
-    visit '/'
-    fill_in :name, with: 'Super Mario'
-    click_button 'Start Game'
+    sign_up_and_play
     expect(page).to have_selector(:link_or_button, 'ROCK')
     expect(page).to have_selector(:link_or_button, 'PAPER')
     expect(page).to have_selector(:link_or_button, 'SCISSORS')
   end
 
   scenario 'player can choose one option' do
-    visit '/'
-    fill_in :name, with: 'Super Mario'
-    click_button 'Start Game'
+    sign_up_and_play
     random_pick = ["ROCK", "PAPER", "SCISSORS"].sample
     click_button random_pick
     expect(page).to have_content("You picked #{random_pick}")
   end
 
   scenario 'game will choose a random option' do
-    visit '/'
-    fill_in :name, with: 'Super Mario'
-    click_button 'Start Game'
+    sign_up_and_play
     srand(4)
     click_button "PAPER"
     expect(page).to have_content("and Darth Vader picked SCISSORS")
   end
 
   scenario 'a winner is declared' do
-    visit '/'
-    fill_in :name, with: 'Super Mario'
-    click_button 'Start Game'
+    sign_up_and_play
     srand(7)
     click_button "PAPER"
     expect(page).to have_content("You WIN!")
   end
 
   scenario 'a draw is declared' do
-    visit '/'
-    fill_in :name, with: 'Super Mario'
-    click_button 'Start Game'
+    sign_up_and_play
     srand(7)
     click_button "ROCK"
     expect(page).to have_content("It's a DRAW!")
   end
 
   scenario 'player can see Play Again button' do
-    visit '/'
-    fill_in :name, with: 'Super Mario'
-    click_button 'Start Game'
+    sign_up_and_play
     click_button "ROCK"
     expect(page).to have_selector(:link_or_button, 'Play Again')
   end
 
   scenario 'player can play again' do
-    visit '/'
-    fill_in :name, with: 'Super Mario'
-    click_button 'Start Game'
+    sign_up_and_play
     click_button "ROCK"
     click_button "Play Again"
     expect(page).to have_selector(:link_or_button, 'ROCK')
