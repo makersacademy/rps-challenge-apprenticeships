dbbaefba7771605a9c7e3ebdb5d0e7c526bd74af
Arran Oakes <arran.oakes@gmail.com>
2021-06-18 10:18:41 +0100

    redesigned landing page to allow for multiplayer option later and amended existing feature test accordingly

--- app.rb
+++ app.rb
@@ -10,6 +10,16 @@ class RockPaperScissors < Sinatra::Base
     erb :index
   end
 
+  post '/mode-select' do
+    choice = params[:name]
+    redirect('/two-player') if choice == "2P"
+    redirect('/one-player')
+  end
+
+  get '/one-player' do
+    erb :singleplayermenu
+  end
+
   post '/name-submit' do
     session[:name] = params[:name]
     redirect('/welcome')
--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -8,6 +8,7 @@ feature 'user can register their name' do
   let(:name) { double("name") }
   scenario 'the user can submit and return their name' do
     visit('/')
+    click_button("1P")
     fill_in :name, with: name
     click_button("Start")
     expect(page).to have_content name
