de5ee4484fea2ca5387e14ad8cdd07056a84cfc0
Arran Oakes <arran.oakes@gmail.com>
2021-06-18 11:59:18 +0100

    computer has a name

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,3 +1,10 @@
 class Computer
 
+  attr_reader :name
+
+  def initialize
+    @name = ["Deep Thought"].sample
+  end
+
+
 end
--- spec/units/computer_spec.rb
+++ spec/units/computer_spec.rb
@@ -1,5 +1,8 @@
 require_relative '../../lib/computer'
 
 describe Computer do
+  it 'has a name' do
+    expect(subject.name).to_not be_empty
+  end
 
 end
