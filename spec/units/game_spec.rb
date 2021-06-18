8472a51ff8442566b00680dfc312846ecc017ec2
Arran Oakes <arran.oakes@gmail.com>
2021-06-18 12:26:32 +0100

    tested Computer class move method through Game

--- spec/units/game_spec.rb
+++ spec/units/game_spec.rb
@@ -1,14 +1,14 @@
 require_relative '../../lib/game'
 
 describe Game do
-subject(:object) { described_class }
+subject(:object) { described_class.new(player_1) }
 let(:player_1) { double(:player_1)}
 let(:computer) { class_double(Computer) }
 
 # let(:pve_game) { Game.new(player_1, computer) }
 
   it "starts a game" do
-    expect(object).to respond_to(:new)
+    expect(Game).to respond_to(:new)
   end
 
   context "when playing a single player game" do
@@ -20,7 +20,11 @@ let(:computer) { class_double(Computer) }
       it 'adds a computer player' do
         pve_game = Game.new(player_1, computer)
         expect(pve_game.player_2).to eq(computer)
+      end
 
+      it 'allows the computer to make a choice' do
+        srand(4)
+        expect(subject.player_2.make_move).to eq "Scissors"
       end
 
   end
