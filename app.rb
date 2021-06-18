b504c3bf5302082d858f486ebcf16ef7d0629839
ehelsan <ehelsan@hotmail.co.uk>
2021-06-18 12:24:32 +0100

    name form done

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
-require 'sinatra/base'
+require 'sinatra'
+require 'sinatra/reloader' if development?
 class RockPaperScissors < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  enable:sessions
+
   get '/test' do
     'test page'
+  end
+  get '/' do
+    erb(:name_form)
+  end
+  post '/name' do
+    session[:name_one] = params[:Name_one]
   end
 
   run! if app_file == $0
---
+++ spec/features/name_form_spec.rb
@@ -0,0 +1,8 @@
+feature 'allows marketeer to register name before playing an online game' do
+  scenario 'input name of Player' do
+    visit '/'
+    fill_in :Name_one, with: "Ehelsan"
+    click_button('Submit')
+    expect(page).to have_content "Ehelsan"
+  end
+end
