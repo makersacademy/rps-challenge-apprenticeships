ae601cfac685536777798a689839ef272d1870b7
anitaRead <areadman91@gmail.com>
2021-06-18 12:01:01 +0100

    Add web_helper 'sign_in_and_play'

--- spec/features/play_rps_spec.rb
+++ spec/features/play_rps_spec.rb
@@ -1,24 +1,18 @@
 feature 'play rock paper scissors' do
     scenario 'play rock' do
-      visit '/'
-      fill_in('player', with: 'Anita')
-      click_button('submit')
+      sign_in_and_play
       choose(option: 'rock')
       click_button('submit')
       expect(page).to have_content 'Anita played: rock'
     end
     scenario 'play paper' do
-        visit '/'
-        fill_in('player', with: 'Anita')
-        click_button('submit')
+        sign_in_and_play
         choose(option: 'paper')
         click_button('submit')
         expect(page).to have_content 'Anita played: paper'
     end
     scenario 'play scissors' do
-        visit '/'
-        fill_in('player', with: 'Anita')
-        click_button('submit')
+        sign_in_and_play
         choose(option: 'scissors')
         click_button('submit')
         expect(page).to have_content 'Anita played: scissors'
