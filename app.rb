8118131666553e22e20493751cda72935c8e9fa8
Jack <jackhalladay@gmail.com>
2021-08-06 10:29:14 +0100

    Player can enter their name

--- app.rb
+++ app.rb
@@ -5,5 +5,10 @@ class RockPaperScissors < Sinatra::Base
     erb :index
   end
 
+  post '/name' do
+    @player_1_name = params[:player_1_name]
+    erb :play
+  end
+
   run! if app_file == $0
 end
--- spec/features/test_page_spec.rb
+++ spec/features/test_page_spec.rb
@@ -1,6 +1,6 @@
 feature 'Home page runs' do
   scenario 'visit home page' do
     visit '/'
-    expect(page).to have_content('Please enter your name!')
+    expect(page).to have_content('Rock, Paper, Scissors!')
   end
 end
