22b8f8ab05abf785470cd4c5273476bb56ebc6db
manu <manuelmoualek@gmail.com>
2021-08-06 10:44:03 +0100

    Adding name functionality added for 1 player

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
+
 class RockPaperScissors < Sinatra::Base
+  
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    @player_1 = params[:player_1]
+    erb :play
+  end
+
   get '/test' do
     'test page'
   end
--- spec/features/test_page_spec.rb
+++ spec/features/test_page_spec.rb
@@ -1,6 +1,16 @@
+
 feature 'test page' do
   scenario 'visit test page' do
     visit '/test'
     expect(page).to have_content('test page')
   end
+
+  scenario 'two players can add their names for a personalised game' do
+    visit "/"
+    fill_in('player_1', with: 'Player 1')
+    click_on('Submit')
+    expect(page).to have_content "Player 1 vs. CPU"
+  end
 end
+
+
