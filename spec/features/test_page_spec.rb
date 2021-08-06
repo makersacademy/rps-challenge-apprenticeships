5c6e9fde32f21eae1c18439f81d9b2c595ffc75f
manu <manuelmoualek@gmail.com>
2021-08-06 12:17:29 +0100

    player can select choice thanks to radio buttons

--- spec/features/test_page_spec.rb
+++ spec/features/test_page_spec.rb
@@ -5,12 +5,30 @@ feature 'test page' do
     expect(page).to have_content('test page')
   end
 
-  scenario 'two players can add their names for a personalised game' do
+  scenario 'player can add their name for a personalised game' do
     visit "/"
     fill_in('player_1', with: 'Player 1')
-    click_on('Submit')
+    click_on('Play Game')
     expect(page).to have_content "Player 1 vs. CPU"
   end
+
+  scenario 'player can click either rock, paper, or scissors buttons' do
+    visit "/"
+    fill_in('player_1', with: 'Player 1')
+    click_on('Play Game')
+    choose('rock')
+    choose('paper')
+    choose('scissors')
+  end
+
+  # scenario 'player selects either rock, paper, or scissors by clicking corresponding button'
+  #   visit "/"
+  #   fill_in('player_1', with: 'Player 1')
+  #   click_on('Submit')
+  #   find_button('Rock').click
+  #   find_button('Paper').click
+  #   find_button('Scissors').click
+  # end
 end
 
 
