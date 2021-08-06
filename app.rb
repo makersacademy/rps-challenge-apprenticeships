b2491c0d1d7c7aa0e37705359f73694afbf66b51
Jack <jackhalladay@gmail.com>
2021-08-06 11:16:28 +0100

    edit

--- app.rb
+++ app.rb
@@ -1,12 +1,17 @@
 require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
-  
+  enable :sessions
   get '/' do
     erb :index
   end
 
   post '/name' do
-    @player_1_name = params[:player_1_name]
+    session[:player_1_name] = params[:player_1_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
     erb :play
   end
 
