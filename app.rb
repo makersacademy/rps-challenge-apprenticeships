6320a784fff1f2eaf06af9e0ac70590c623e91fd
Roisin <roisinmaguire@ma359.home>
2021-08-06 11:46:54 +0100

    player can submit name

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
+
 class RockPaperScissors < Sinatra::Base
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    erb :registername
+  end
+
   get '/test' do
     'test page'
   end
