50d1f6c96359df316aa9b19c8eb573c3cd0b1bdc
hjdalton <hjdalton@hotmail.co.uk>
2021-08-06 12:03:32 +0100

    random method created and tests passing. initialized instance variables

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,18 @@
 class Game
+  attr_reader :player_move
+
+  def initialize(player_move)
+    @player_move
+    @computer_move = ""
+    @options = ["Rock", "Paper", "Scissors"]
+  end
+
   def present
     "Rock, Paper, Scissors"
   end
 
   def random
-    true
+    move = @options.sample
+    @computer_move << move
   end
 end
--- spec/units/game_spec.rb
+++ spec/units/game_spec.rb
@@ -1,25 +1,28 @@
 require './lib/game.rb'
 
 describe Game do
-  describe '#present_' do
-    it 'presents choices to the player' do
-      expect(subject.present).to eq "Rock, Paper, Scissors"
-    end
-  end
+  subject(:play) {described_class.new("Rock")}
+  # describe '#present_' do
+  #   it 'presents choices to the player' do
+  #     expect(subject.present).to eq "Rock, Paper, Scissors"
+  #   end
+  # end
 
-  pending describe '#player_choice' do
-    it 'the player chooses their move' do     
-    end
-  end
+  # pending describe '#player_move' do
+  #   it 'the player chooses their move' do     
+  #   end
+  # end
 
   describe '#random' do
     it 'chooses a random option' do
-      expect(subject.random).to be true
+      allow(play).to receive(:random).and_return("Rock")
+      expect(play.random).to eq "Rock"
     end
   end
 
-  pending describe '#winner' do 
-    it 'presents the winner' do
-    end
-  end 
+  # pending describe '#winner' do 
+  #   it 'presents the winner' do
+  #     expect(subject.winner).to 
+  #   end
+  # end 
 end
