8c248b4651aadc6f6ddf03f82f8b3e11a1d34900
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-07 12:16:19 +0100

    feat(player): implement ability to set the players choice as an instance variable

--- spec/lib/player_spec.rb
+++ spec/lib/player_spec.rb
@@ -7,4 +7,12 @@ describe Player do
       expect(player.name).to eq "foo"
     end
   end
+
+  describe "#set_choice" do
+    it "sets the players choice to what is passed" do
+      player = Player.new('foo')
+      player.set_choice('Rock')
+      expect(player.choice).to eq 'Rock'
+    end
+  end
 end
