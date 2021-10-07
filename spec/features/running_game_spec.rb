a648f9b21265450b1457bd4606461db792d9e1f2
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-07 13:54:17 +0100

    refactor(*): use helper for running_game_spec

--- spec/features/running_game_spec.rb
+++ spec/features/running_game_spec.rb
@@ -2,41 +2,17 @@ require 'game'
 
 feature 'game page' do
   scenario 'user wins' do
-    # stub to ensure consistency of random method in testing
-    allow_any_instance_of(Game).to receive(:computer_choice).and_return("Scissors")
-    
-    user_starts_game
-    select "Rock", from: "choice"
-    click_button "Go!"
-
-    expect(page).to have_content "You entered: Rock"
-    expect(page).to have_content("Computer entered: Scissors")
+    run_game('Rock')
     expect(page).to have_content "Player wins!"
   end
 
   scenario 'computer wins' do
-    # stub to ensure consistency of random method in testing
-    allow_any_instance_of(Game).to receive(:computer_choice).and_return("Scissors")
-    
-    user_starts_game
-    select "Paper", from: "choice"
-    click_button "Go!"
-
-    expect(page).to have_content "You entered: Paper"
-    expect(page).to have_content("Computer entered: Scissors")
+    run_game('Paper')
     expect(page).to have_content "Computer wins!"
   end
 
   scenario 'draw' do
-    # stub to ensure consistency of random method in testing
-    allow_any_instance_of(Game).to receive(:computer_choice).and_return("Scissors")
-    
-    user_starts_game
-    select "Scissors", from: "choice"
-    click_button "Go!"
-
-    expect(page).to have_content "You entered: Scissors"
-    expect(page).to have_content("Computer entered: Scissors")
+    run_game('Scissors')
     expect(page).to have_content "It's a draw!"
   end
 
