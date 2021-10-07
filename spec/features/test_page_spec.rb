4aad9d39e945674ad29dd3939c778dee7892822e
Kirsty Harris <kittyharris5@yahoo.co.uk>
2021-10-07 21:09:08 +0100

    enter name test added

--- spec/features/test_page_spec.rb
+++ spec/features/test_page_spec.rb
@@ -4,3 +4,12 @@ feature 'test page' do
     expect(page).to have_content('test page')
   end
 end
+
+feature 'enter name' do
+  scenario 'regsiter name' do
+    vist ('/')
+    fill_in :player_1_name, with: 'Kirsty'
+    click_button 'Submit'
+    expect(page).to have_content 'Kirsty'
+  end
+end
