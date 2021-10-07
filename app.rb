31ff29a88388801468b809da983f8534407e7424
Kirsty Harris <kittyharris5@yahoo.co.uk>
2021-10-07 22:37:17 +0100

    Player 1 name can be added - error with spec_helper

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
+
   get '/test' do
     'test page'
   end
 
-  run! if app_file == $0
-end
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    erb :play
+  end
+
--- spec/features/test_page_spec.rb
+++ spec/features/test_page_spec.rb
@@ -4,12 +4,3 @@ feature 'test page' do
     expect(page).to have_content('test page')
   end
 end
-
-feature 'enter name' do
-  scenario 'regsiter name' do
-    vist ('/')
-    fill_in :player_1_name, with: 'Kirsty'
-    click_button 'Submit'
-    expect(page).to have_content 'Kirsty'
-  end
-end
