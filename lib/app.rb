3bfacefbfcffcb2fa5d642bdcdc7a233a7cc992f
FrancineBlanc <francine.blanc@gmail.com>
2021-10-07 13:52:03 +0100

    a user can enter their name

--- lib/app.rb
+++ lib/app.rb
@@ -2,10 +2,12 @@ require "sinatra/base"
 require "sinatra/reloader"
 
 class RockPaperScissors < Sinatra::Base
-
+  use Rack::Session::Cookie
   configure :development do
     register Sinatra::Reloader
   end
+
+  enable :sessions
 
   get '/test' do
     'test page'
@@ -13,6 +15,16 @@ class RockPaperScissors < Sinatra::Base
 
   get "/" do
     erb(:index)
+  end
+
+  post "/name-info" do
+    session[:username] = params[:name]
+    redirect "/play"
+  end
+
+  get "/play" do
+    @name = session[:username]
+    erb(:play)
   end
 
   run! if app_file == $0
--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -1,6 +1,19 @@
 feature "the home page" do
-  scenario "the home page returns a successful status code" do
+  before(:each) do
     visit "/"
+  end
+
+  scenario "the home page returns a successful status code" do
     expect(page.status_code).to eq 200
+  end
+
+  scenario "the home page contains a title" do
+    expect(page).to have_content "Rock, Paper, Scissors"
+  end
+
+  scenario "a user can enter their name" do
+    fill_in "name", with: "Tina"
+    click_button("Let's play!")
+    expect(page).to have_content "Welcome Tina!"
   end
 end
