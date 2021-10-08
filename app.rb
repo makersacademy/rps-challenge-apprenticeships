625fc70267aa68d884ab37bd0693fe768801f568
michelle <michelle.watkins@engineering.digital.dwp.gov.uk>
2021-10-08 14:33:51 +0100

    removing comments

--- app.rb
+++ app.rb
@@ -2,9 +2,6 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class RockPaperScissors < Sinatra::Base
-  # configure :development do
-  #   register Sinatra::Reloader
-  # end
   enable :sessions
 
   get '/' do
