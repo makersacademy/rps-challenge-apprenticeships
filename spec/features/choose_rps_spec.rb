0cb23449f20cf28c8ef9591c6d354649870338d3
Leo Brown <leo_brown_1@hotmail.com>
2021-10-08 14:10:17 +0100

    Added 3 buttons to selection

--- spec/features/choose_rps_spec.rb
+++ spec/features/choose_rps_spec.rb
@@ -1,7 +1,11 @@
 feature "choose game option" do
-  scenario "allows the player to choose rock paper or scissors and see on screen" do
+  scenario "allows the player to choose rock paper or scissors" do
     visit "/play"
-    # fill_in("selection", with: "Rock")
+    expect(page).to have_field "selection"
+  end
+
+  scenario "allows the player to choose one option and see on screen" do
+    visit "/play"
     choose id = "Rock"
     click_button ["Submit"]
     expect(page).to have_content "You chose: Rock"
