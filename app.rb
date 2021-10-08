2277d8682002642112a0c5274d17a3f373736b87
Patrick Chan <q_p18@hotmail.com>
2021-10-08 11:12:02 +0100

    Require 'sinatra/reloader', enable sessions, create routes for '/', '/name' and '/game'

--- app.rb
+++ app.rb
@@ -1,7 +1,25 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
+
 class RockPaperScissors < Sinatra::Base
-  get '/test' do
-    'test page'
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  enable :sessions
+
+  get '/' do
+    erb :index
+  end
+
+  post '/name' do
+    session[:player_name] = params[:player_name]
+    redirect '/game'
+  end
+
+  get '/game' do
+    @player_name = session[:player_name]
+    erb :game
   end
 
   run! if app_file == $0
