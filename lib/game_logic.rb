3ea88c3ae4080af9b8960fc454ea0226b5d781c7
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-08 12:52:13 +0100

    feat(game_logic): add extended game logic

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -11,5 +11,23 @@ module GameLogic
     choice1 == choice2
   end
 
-  # def self.player1_wins_extended_game?(choice1, choice2)
+  def self.player1_wins_extended_game?(choice1, choice2)
+    true if (
+      player1_wins?(choice1, choice2) ||
+      spock(choice1, choice2) ||
+      choice1 == "Rock" && choice2 == "Lizard" ||
+      choice1 == "Scissors" && choice2 == "Lizard" ||
+      choice1 == "Lizard" && choice2 == "Paper" ||
+      choice1 == "Paper" && choice2 == "Spock"
+    )
+  end
+
+  private_class_method
+
+  def self.spock(choice1, choice2)
+    true if (
+      choice1 == "Spock" && choice2 == "Scissors" ||
+      choice1 == "Spock" && choice2 == "Rock"
+    )
+  end
 end
