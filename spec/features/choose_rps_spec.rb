5db23ee7c710224f728a65ee32e5114a94de8b3a
Leo Brown <leo_brown_1@hotmail.com>
2021-10-08 13:34:59 +0100

    Changed textbox entry to radio button

--- spec/features/choose_rps_spec.rb
+++ spec/features/choose_rps_spec.rb
@@ -1,8 +1,9 @@
 feature "choose game option" do
   scenario "allows the player to choose rock paper or scissors and see on screen" do
     visit "/play"
-    fill_in("selection", with: "Rock")
-    click_button "Submit"
-    expect(page).to have_content "You chose Rock"
+    # fill_in("selection", with: "Rock")
+    choose id = "Rock"
+    click_button ["Submit"]
+    expect(page).to have_content "You chose: Rock"
   end
 end
