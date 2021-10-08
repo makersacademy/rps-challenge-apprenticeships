4f39e919013c4cf5d2b09a05d5aa36432f290131
Sunny Donaldson <sunnydonaldson1@gmail.com>
2021-10-08 14:12:38 +0100

    declaring a winner

--- app.rb
+++ app.rb
@@ -25,9 +25,20 @@ class RockPaperScissors < Sinatra::Base
   end
 
   get "/turn" do
+    @move_hierachy ={rock: :scissors, scissors: :paper, paper: :rock}
     @name = $name
     @player_choice = $player_choice
-    @bot_choice = ["rock","paper","scissors"].sample
+    @bot_choice = [:rock,:paper,:scissors].sample
+    
+    if(@move_hierachy[@bot_choice]==@player_choice.to_sym)
+      @winner = "bot"
+      @loser = @name
+    elsif(@move_hierachy[@user_choice]==@bot_choice)
+      @winner = @name
+      @loser = bot
+    else
+      @draw = true
+    end
     return erb(:turn)
   end
 
