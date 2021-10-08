a67ddf3636af1c5458bc98ff4eab5a7af8e18783
Leo Brown <leo_brown_1@hotmail.com>
2021-10-08 18:56:50 +0100

    Added sequence diagram to folder

--- spec/features/ai_choice_spec.rb
+++ spec/features/ai_choice_spec.rb
@@ -5,4 +5,11 @@ feature "ai chooses game option" do
     click_button ["Submit"]
     expect(page).to have_content "The AI chose: Paper"
   end
+
+  # scenario "allows the ai generates a random choice" do
+  #   visit "/play"
+  #   choose id = "Rock"
+  #   click_button ["Submit"]
+  #   expect(page).to have_content "The AI chose: Paper"
+  end
 end
