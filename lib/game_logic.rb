2181004f5e1dfe9a5bd2cb4b663c055872571eeb
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-08 14:01:50 +0100

    refactor(game_logic): delete method

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -14,18 +14,12 @@ module GameLogic
   def self.player1_wins_extended_game?(choice1, choice2)
     true if (
       player1_wins?(choice1, choice2) ||
-      spock(choice1, choice2) ||
+      choice1 == "Spock" && choice2 == "Scissors" ||
+      choice1 == "Spock" && choice2 == "Rock" ||
       choice1 == "Rock" && choice2 == "Lizard" ||
       choice1 == "Scissors" && choice2 == "Lizard" ||
       choice1 == "Lizard" && choice2 == "Paper" ||
       choice1 == "Paper" && choice2 == "Spock"
-    )
-  end
-
-  def self.spock(choice1, choice2)
-    true if (
-      choice1 == "Spock" && choice2 == "Scissors" ||
-      choice1 == "Spock" && choice2 == "Rock"
     )
   end
 end
