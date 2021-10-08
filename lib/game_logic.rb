da8bdd7c4c322445c8eecf557b3424887f5b190c
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-08 12:35:41 +0100

    refactor(game_logic): rename method variables to reflect reusability of module

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,13 +1,15 @@
 module GameLogic
-  def self.player1_wins?(player_choice, computer_choice)
+  def self.player1_wins?(choice1, choice2)
     true if (
-    player_choice == "Scissors" && computer_choice == "Paper" ||
-    player_choice == "Rock" && computer_choice == "Scissors" ||
-    player_choice == "Paper" && computer_choice == "Rock"
+    choice1 == "Scissors" && choice2 == "Paper" ||
+    choice1 == "Rock" && choice2 == "Scissors" ||
+    choice1 == "Paper" && choice2 == "Rock"
   )
   end
   
-  def self.draw?(player_choice, computer_choice)
-    player_choice == computer_choice
-  end  
+  def self.draw?(choice1, choice2)
+    choice1 == choice2
+  end
+
+  # def self.player1_wins_extended_game?(choice1, choice2)
 end
