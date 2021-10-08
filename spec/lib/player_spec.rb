9e6626f7bb7b7c71096d4509c147bd86b2fc978c
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-08 14:16:15 +0100

    refactor(player_spec): dry tests

--- spec/lib/player_spec.rb
+++ spec/lib/player_spec.rb
@@ -1,18 +1,18 @@
 require 'player'
 
 describe Player do
+  before(:each) {@player = Player.new('foo') }
+
   describe "#initialize" do
     it "is initialised with a name" do
-      player = Player.new("foo")
-      expect(player.name).to eq "foo"
+      expect(@player.name).to eq "foo"
     end
   end
 
   describe "#move" do
     it "sets the players choice to what is passed" do
-      player = Player.new('foo')
-      player.move('Rock')
-      expect(player.choice).to eq 'Rock'
+      @player.move('Rock')
+      expect(@player.choice).to eq 'Rock'
     end
   end
 end
