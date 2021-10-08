deff3f086ef7217619d725285246cd3cffe5ee96
Dan Marshall <daniel.o.marshall@gmail.com>
2021-10-08 12:39:57 +0100

    Added user choice and web helper

--- app.rb
+++ app.rb
@@ -10,6 +10,11 @@ class RockPaperScissors < Sinatra::Base
     erb :index
   end
 
+  post '/choose-your-weapon' do
+    @player_1_name = params[:player_1_name]
+    erb :play
+  end
+
   get '/test' do
     'test page now reloads'
   end
--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -3,4 +3,9 @@ feature 'homepage' do
     visit '/'
     expect(page).to have_content('Ready to play Rock, Paper, Scissors?')
   end
+
+  scenario 'user enters name' do
+    sign_in_and_play
+    expect(page).to have_content 'Player 1: Dan'
+  end
 end
