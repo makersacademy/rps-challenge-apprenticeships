c977d661f80da0ee7c02e74deb3c7062e3c24e94
Beth Lambert-Matthews <blambertmatthews@gmail.com>
2021-10-08 11:45:53 +0100

    Updated Gemfile and spec helper. Confirmed Ruby version. Added views folder

---
+++ BethREADME.md
@@ -0,0 +1,10 @@
+Setup code
+Bundle install
+Rspec
+Update Spec Helper
+
+Look for folders and empty files
+spec folder, features
+
+Also app.rb already there
+
--- app.rb
+++ app.rb
@@ -1,8 +1,23 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
+
 class RockPaperScissors < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  # enable :sessions
+
+  get '/' do
+    erb :index
+  end
+
   get '/test' do
     'test page'
   end
 
-  run! if app_file == $0
+  # run! if app_file == $0
+  # Don't understand the syntax of this line above
+  # Was already in the app.rb file
+
 end
