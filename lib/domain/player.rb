1be0b167dc7a4b04ac4bb3c025e4185fb73c55a9
Steve Jones <steve.jones763@gmail.com>
2021-10-08 10:45:27 +0100

    Player object created with name property and can add wins and losses.

---
+++ lib/domain/player.rb
@@ -0,0 +1,28 @@
+class Player
+
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @games_record = {:wins => 0, :losses => 0}
+  end
+
+  def wins
+    @games_record[:wins]
+  end
+
+  def losses
+    @games_record[:losses]
+  end
+
+  def total_games_played
+    wins + losses
+  end
+
+  def add_win
+    @games_record[:wins] += 1
+  end
+
+  def add_loss
+    @games_record[:losses] += 1
+  end
+end
---
+++ spec/unit/domain/player_spec.rb
@@ -0,0 +1,55 @@
+require 'domain/player'
+
+describe Player do
+  let(:name) { "Harry" }
+  let(:subject) { described_class.new(name)}
+  it 'has a name' do
+    expect(subject.name).to eq name
+  end
+
+  describe '#wins' do
+    it 'starts with 0 wins' do
+      expect(subject.wins).to eq 0
+    end
+
+    it 'equals 5 after 5 wins' do
+      5.times { subject.add_win }
+      expect(subject.wins).to eq 5
+    end
+  end
+
+  describe '#add_win' do
+    it 'adds a win to the players record' do
+      expect {subject.add_win}.to change {subject.wins}.by 1
+    end
+  end
+
+  describe '#add_loss' do
+    it 'adds a win to the players record' do
+      expect {subject.add_loss}.to change {subject.losses}.by 1
+    end
+  end
+
+  describe '#losses' do
+    it 'starts with 0 losses' do
+      expect(subject.losses).to eq 0
+    end
+
+    it 'equals 5 after 5 losses' do
+      5.times { subject.add_loss }
+      expect(subject.losses).to eq 5
+    end
+  end
+
+  describe '#total_games_played' do
+    it 'starts with 0 total games played' do
+      expect(subject.total_games_played).to eq 0
+    end
+    it 'equals 10 after 5 wins and losses' do
+      5.times { subject.add_win }
+      5.times { subject.add_loss }
+      expect(subject.total_games_played).to eq 10
+    end
+  end
+
+end
