474cb9818aa914ff07e4368feaa8655d6d9275d0
Dan Marshall <daniel.o.marshall@gmail.com>
2021-10-08 11:39:01 +0100

    Installed webrick and sinatra reloader

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
+
 class RockPaperScissors < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
   get '/test' do
-    'test page'
+    'test page now reloads'
   end
 
   run! if app_file == $0
