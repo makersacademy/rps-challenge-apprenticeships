44832f8e4528b344e907629208877f15fe6a0343
Patrick Chan <q_p18@hotmail.com>
2021-10-08 11:35:20 +0100

    Refactor with web helper method

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,8 +1,6 @@
 feature "Enter name" do
   scenario "Before game starts, player enters name" do
-    visit('/')
-    fill_in 'player_name', with: 'John'
-    click_button 'Register'
+    register
 
     expect(page).to have_content 'John'
   end
