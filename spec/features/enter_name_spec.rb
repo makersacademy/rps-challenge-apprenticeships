265322bdbed6b2a5497c7764f2256e0e1976a816
frontendowl <frontendowl@gmail.com>
2021-10-08 12:01:10 +0100

    Add input field

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -3,4 +3,9 @@ feature 'entering name' do
     visit "/"
     expect(page.status_code).to eq 200
   end
+
+  scenario 'page has input field for name' do
+    visit "/"
+    expect(page).to have_field "player"
+  end
 end
