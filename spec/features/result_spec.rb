809e9ed09e3775b81baf5934d60e133e31b393a0
Anamaria-Mocanu <Annamaariaaa@yahoo.com>
2021-10-09 17:27:49 +0100

    finished incorporating the game.rb in the website and added a try again button

--- spec/features/result_spec.rb
+++ spec/features/result_spec.rb
@@ -1,16 +1,26 @@
 feature "Results" do
-  let(:player) { instance_double(Player, :name => "Ana") }
-  let(:rock) { instance_double(RockPaperScissors, :rock_option => "rock") }
-  let(:paper) { instance_double(RockPaperScissors, :paper_option => "paper") }
-  let(:scissors) { instance_double(RockPaperScissors, :scissors_option => "scissors") }
-  scenario "shows the result of choosing rock" do
-    # game = Game.new(rock.rock_option, player)
-    # expect_any_instance_of(Kernel).to receive(:rand).with(3).and_return(0)
-    # allow(:player_choice).to receive(:rand).with(3).and_return(0)
+  scenario "shows the try again button to return to start page" do
     visit ('/names')
     fill_in :player_name, with: "Ana"
     click_button "Start game"
     click_button("Rock")
-    expect(page).to have_content $player_choice.round_player_rock
+    click_button("Try again")
+    expect(page).to have_content "Hello Ana, welcome to Rock/Paper/Scissors"
+  end
+  scenario "shows the try again button to return to start page" do
+    visit ('/names')
+    fill_in :player_name, with: "Ana"
+    click_button "Start game"
+    click_button("Paper")
+    click_button("Try again")
+    expect(page).to have_content "Hello Ana, welcome to Rock/Paper/Scissors"
+  end
+  scenario "shows the try again button to return to start page" do
+    visit ('/names')
+    fill_in :player_name, with: "Ana"
+    click_button "Start game"
+    click_button("Scissors")
+    click_button("Try again")
+    expect(page).to have_content "Hello Ana, welcome to Rock/Paper/Scissors"
   end
 end
