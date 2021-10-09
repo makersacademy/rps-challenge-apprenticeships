33b55dd2d71a4f74c45e2a9963167fcdabd59cbf
parsa <parsamesg@gmail.com>
2021-10-09 20:15:30 +0100

    add sinatra-reloader

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require "sinatra/base"
+require "sinatra/reloader"
 
 class RockPaperScissors < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get "/" do
+    "Welcome to Rock, Paper Scissors!"
+  end
 end
