529d34f479a0c6bd4a945c6d937e277fcf02090a
Sunny Donaldson <sunnydonaldson1@gmail.com>
2021-10-10 16:59:48 +0100

    integrating game class with web app, refactoring to remove global variables

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra/base'
+require_relative "./lib/game.rb"
+
 class RockPaperScissors < Sinatra::Base
   get '/test' do
     'test page'
@@ -9,36 +11,30 @@ class RockPaperScissors < Sinatra::Base
   end
 
   post "/play" do
-    $name = params[:name]
+    Game.new(params[:name])
     redirect("/play")
   end
 
   get "/play" do
-    @name = $name
+    @name = Game.current_game.name
     erb(:play)
   end
 
   post "/turn" do
-    $player_choice = params[:button]
+    Game.current_game.user_turn(params[:button].to_sym)
     redirect("/turn")
     
   end
 
   get "/turn" do
-    @move_hierachy ={rock: :scissors, scissors: :paper, paper: :rock}
-    @name = $name
-    @player_choice = $player_choice
-    @bot_choice = [:rock,:paper,:scissors].sample
+    @name = Game.current_game.name()
+    @player_choice = Game.current_game.user_choice()
+    @bot_choice = Game.current_game.bot_turn()
     
-    if(@move_hierachy[@bot_choice]==@player_choice.to_sym)
-      @winner = "the bot"
-      @loser = @name
-    elsif(@move_hierachy[@player_choice.to_sym]==@bot_choice)
-      @winner = @name
-      @loser = "the bot"
-    else
-      @draw = true
-    end
+    Game.current_game.declare_winner()
+    @winner = Game.current_game.winner()
+    @loser = Game.current_game.loser()
+    @draw = Game.current_game.draw()
     return erb(:turn)
   end
 
