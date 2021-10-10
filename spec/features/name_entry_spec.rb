916f24dadd5951bc7e3243a1b6a682c91627246b
parsa <parsamesg@gmail.com>
2021-10-10 20:20:07 +0100

    create web helper for refactoring

--- spec/features/name_entry_spec.rb
+++ spec/features/name_entry_spec.rb
@@ -1,8 +1,6 @@
 feature "enter player name" do
   scenario "user enters a player name" do
-    visit "/"
-    fill_in "player_name", with: "Steve"
-    click_button("Submit")
+    sign_in_and_play
     expect(page).to have_content("Steve VS Computer")
   end
 end
