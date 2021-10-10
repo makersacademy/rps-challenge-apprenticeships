1e66a00f32ff9434ef5b07aa16b1f4bc3944f236
Maisha <maisha-1998@hotmail.co.uk>
2021-10-10 11:01:26 +0100

    uploaded image

--- docs/lib/app.rb
+++ docs/lib/app.rb
@@ -1,2 +1,16 @@
-require 'sinatra/base'
-require 'sinatra/reloader'
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+get '/' do
+  'hello!'
+end
+
+get '/secret' do
+  'This is a secret page'
+end
+
+get '/game' do
+  "<div>
+    <img src='https://facts.net/wp-content/uploads/2020/11/rock-paper-scissors-730x487.jpg'>
+   </div>"
+end
