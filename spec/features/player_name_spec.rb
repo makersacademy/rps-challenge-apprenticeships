e96a6855d1ad3d7cb185ebb56d8019bcc0d8dc47
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-10-29 13:04:43 +0100

    changed submit button name to start game

--- spec/features/player_name_spec.rb
+++ spec/features/player_name_spec.rb
@@ -2,8 +2,8 @@ feature 'Player registration' do
   scenario 'Player inputs name in form' do
     visit '/'
     fill_in :player1, with: 'Rich'
-    click_button 'Submit'
+    click_button 'Start game'
 
-    expect(page).to have_content('')
+    # expect(page).to have_content("Let's go, Rich")
   end
 end
