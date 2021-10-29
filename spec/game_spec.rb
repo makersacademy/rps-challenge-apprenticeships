896dc4dad5621919a4fe047d87fee46147af314c
ScottDonovan <spdonovan@hotmail.co.uk>
2021-10-29 12:28:16 +0100

    Player and Game classes initialized

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,16 @@
 require 'game'
 
 describe Game do
+
+  let(:game) { described_class.new(player) }
+
+  let(:player) { double :player }
+
+  describe '#initialization' do
+    it 'retrieves the player' do
+      expect(game.player).to eq player
+    end
+  end 
+
+
 end
