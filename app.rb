f9bd42c7452e15d6d45412c86f9688937afb96b9
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-10-29 14:44:23 +0100

    fixed failing player name feauture test

--- app.rb
+++ app.rb
@@ -1,17 +1,21 @@
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
+
   enable :sessions
+  
   get '/' do
     erb :index
   end
 
-  # get '/play' do
-  # end
+  get '/play' do
+    @player_1 = session[:player_1]
+    erb :play
+  end
 
   post '/names' do
-    session[player_1] = params[:player_1]
-    erb :play
+    session[:player_1] = params[:player_1]
+    redirect '/play'
   end
 
   run! if app_file == $0
