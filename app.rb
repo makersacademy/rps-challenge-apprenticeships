be0b9c14f3b2cc7f21257a736c605411b297dc99
Rebecca <Beckyrose200@aol.com>
2021-10-29 13:47:06 +0100

    First commit

--- app.rb
+++ app.rb
@@ -1,8 +1,30 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
+
 class RockPaperScissors < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  enable :sessions
+
   get '/test' do
     'test page'
   end
+
+  get '/' do
+    erb :index
+  end 
+
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    redirect '/play'
+  end 
+
+  get '/play' do 
+    @player_1_name = session[:player_1_name]
+    erb :play
+  end 
 
   run! if app_file == $0
 end
