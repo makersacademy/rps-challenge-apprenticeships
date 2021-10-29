4b27c97f957e12afe778fd87f844d887a0d008fd
michael steinacher <steinacherm@hotmail.co.uk>
2021-10-29 17:30:39 +0100

    fixed rps_choice_page so it passed button check text (to expect the rock button)

--- spec/features/rps_choice_page_spec.rb
+++ spec/features/rps_choice_page_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature 'Rock, paper, scissors choice page' do
-  scenario 'visit rock, paper, scissors choice page' do
+  scenario 'visit choice page' do
     visit '/rps'
     expect(page).to have_button('rock')
   end
