557464568a94988b131a64e7823b5f944b6301da
Jamie Bowers <jamiebowers3@gmail.com>
2021-10-29 11:08:33 +0100

    Implemented SOLO/MULITPLAYER buttons

--- spec/features/register_page_spec.rb
+++ spec/features/register_page_spec.rb
@@ -2,7 +2,8 @@ feature 'Checks register page' do
 
   scenario 'checks we have solo and multiplayer options' do 
     register_page
-    expect(page).to have_content "Player 1:"
+    expect(page).to have_selector(:link_or_button, "SOLO")
+    expect(page).to have_selector(:link_or_button, "MULTIPLAYER")
   end 
 
 end 
