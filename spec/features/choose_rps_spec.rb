4fb72587100c54d93e3b69116b4e34ff5999c335
ScottDonovan <spdonovan@hotmail.co.uk>
2021-10-29 10:12:19 +0100

    Web_helper added for sign-in and play - Green

---
+++ spec/features/choose_rps_spec.rb
@@ -0,0 +1,5 @@
+feature 'Choose Rock, Paper or Scissors' do
+  scenario 'Player chooses Rock' do
+    sign_in_and_play
+  end
+end
--- spec/features/player_name_spec.rb
+++ spec/features/player_name_spec.rb
@@ -1,8 +1,6 @@
 feature 'Enter player name' do
   scenario 'submitting name' do
-    visit('/')
-    fill_in :player_name, with: 'Scott'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Welcome to RPS Scott'
   end
 end
