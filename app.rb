d94a07714592f874e3d67395af6c188bdffe8d02
yasmin <yasmin.jones@digital.justice.gov.uk>
2021-10-29 14:09:36 +0100

    adding players names in views

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
 require 'sinatra/base'
+
 class RockPaperScissors < Sinatra::Base
+  
+
   get '/test' do
     'test page'
   end
+
+  get '/' do 
+    erb :index
+  end 
+
 
   run! if app_file == $0
 end
