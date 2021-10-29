91cf98e302e76c1c59e17e44ff8a014f08a0e6ab
ScottDonovan <spdonovan@hotmail.co.uk>
2021-10-29 14:29:49 +0100

    Choice of weapon added to Player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,11 @@
 class Player
 
   attr_reader :name
+  attr_accessor :weapon
 
-  def initialize(name)
+  def initialize(name, weapon = nil)
     @name = name
+    @weapon = weapon
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,8 +6,9 @@ describe Player do
     it 'returns the name' do
       expect(player.name).to eq "Scott"
     end
+
+    it "Saves the player choice" do
+      expect(player.weapon).to eq nil
+    end
   end
-
-
-
 end
