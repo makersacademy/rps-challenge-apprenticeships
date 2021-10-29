e51d82f3e8b2dcb6605212489e52d5a88d3f2e1e
yasmin <yasmin.jones@digital.justice.gov.uk>
2021-10-29 15:36:36 +0100

    added 2 more passing  tests in the player chooses their fighter feature

--- spec/features/choose_fighter_spec.rb
+++ spec/features/choose_fighter_spec.rb
@@ -6,4 +6,20 @@ feature 'Player chooses their fighter' do
 
         expect(page).to have_content "Let's start!"
     end 
+
+    scenario 'player picks paper' do 
+        sign_in_and_play
+        select "Paper", :from => "fighter"
+        click_button 'GO!'
+
+        expect(page).to have_content "Let's start!"
+    end 
+
+    scenario 'player picks scissors' do 
+        sign_in_and_play
+        select "Scissors", :from => "fighter"
+        click_button 'GO!'
+
+        expect(page).to have_content "Let's start!"
+    end 
 end 
