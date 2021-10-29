2a17b4f1266666283b3fe8f0370f7f360cfda836
ScottDonovan <spdonovan@hotmail.co.uk>
2021-10-29 14:10:34 +0100

    Computer added into game - Green

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,16 @@
 class Game
 
-  def initialize(player)
+  def initialize(player, computer = Computer.new)
     @player = player
+    @computer = computer
   end
 
   def player
     @player
+  end
+
+  def computer
+    @computer
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,13 +2,18 @@ require 'game'
 
 describe Game do
 
-  let(:game) { described_class.new(player) }
+  let(:game) { described_class.new(player, computer) }
 
   let(:player) { double :player }
+  let(:computer) { double :computer}
 
   describe '#initialization' do
     it 'retrieves the player' do
       expect(game.player).to eq player
+    end
+
+    it 'retrieves the computer' do
+      expect(game.computer).to eq computer
     end
   end 
 
