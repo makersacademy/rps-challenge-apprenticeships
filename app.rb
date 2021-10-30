8717d75d49b946db6f72922bcf05b793d8e12b00
Jenaro Garcia <jjegarcia@hotmail.com>
2021-10-30 09:05:56 +0100

    login loading

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
 require 'sinatra/base'
+# require 'sinatra/reloader'
+
 class RockPaperScissors < Sinatra::Base
+  # configure :development do
+  #   register Sinatra::Reloader
+  # end
+  enable :sessions
+
   get '/test' do
     'test page'
   end
-
+  get '/' do
+    erb :index
+  end
   run! if app_file == $0
 end
--- spec/features/test_page_spec.rb
+++ spec/features/test_page_spec.rb
@@ -1,6 +1,13 @@
 feature 'test page' do
   scenario 'visit test page' do
     visit '/test'
+    puts '---------'
+    puts page.html
     expect(page).to have_content('test page')
   end
+  scenario 'visit index should bring login' do
+    visit '/'
+    expect(page).to have_content('login')
+  end
+
 end
