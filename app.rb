5d72f735855fdd345d04331c5b5692dcf80c7edd
Raul <rauldelahera@hotmail.com>
2021-10-31 16:34:33 +0000

    RPS challenge - User Story#1 (register name) completed

--- app.rb
+++ app.rb
@@ -1,8 +1,26 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
+
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
   get '/test' do
     'test page'
   end
 
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    session[:player_name] = params[:player_name]
+    redirect '/play'
+  end
+  
+  get '/play' do
+    @name = session[:player_name]
+    erb :play
+  end
+
+  # start the server if ruby file executed directly
   run! if app_file == $0
 end
