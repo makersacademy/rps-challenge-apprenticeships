8b553f56fbd8a876158e6c1e91df56114d8151de
uburger <ursburger@gmail.com>
2021-11-01 08:00:41 +0100

    Choose an option

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -8,8 +8,14 @@ feature 'play RPS' do
   end
 
   scenario 'show options' do
-    expect(page).to have_content 'Rock'
-    expect(page).to have_content 'Paper'
-    expect(page).to have_content 'Scissors'
+    expect(page).to have_button 'Rock'
+    expect(page).to have_button 'Paper'
+    expect(page).to have_button 'Scissors'
+  end
+
+  scenario 'choose a option' do 
+    click_button 'Rock'
+    expect(page).to have_content 'You chose Rock'
+
   end
 end
