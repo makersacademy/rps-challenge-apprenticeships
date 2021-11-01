9ae01b1b5d22be5e1f2e51a2d0a767bc0c5b3403
PatrickBarsch <PatrickGB@gmx.de>
2021-11-01 08:54:16 +0100

    Changed naming of and in test for win by player1

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,14 +8,14 @@ describe Game do
       game = Game.new(player1, player2)
       expect(game.winner).to eq(:Winner)
     end
-    it 'always determines the correct winner' do
+    it 'determines a win by player one correclty' do
       winning_weapons = [:Rock, :Paper, :Scissors]
       loosing_weapons = [:Scissors, :Rock, :Paper]
-      expected_wins = [:always_wins, :always_wins, :always_wins]
+      expected_wins = [:player1, :player1, :player1]
       actual_wins = []
       winning_weapons.each_with_index do |weapon, i|
-        player1 = double(name: :always_wins, weapon: weapon)
-        player2 = double(name: :always_looses, weapon: loosing_weapons[i])
+        player1 = double(name: :player1, weapon: weapon)
+        player2 = double(name: :player2, weapon: loosing_weapons[i])
         game = Game.new(player1, player2)
         actual_wins << game.winner
       end
