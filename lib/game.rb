cff597dddee35e2b56977d817bad7d7877f2e4cf
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-11-01 09:16:41 +0000

    added game and player unit tests and implemented code

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,12 @@
+# require 'player'
+
 class Game
+  attr_reader :players
+  def initialize(player_1)
+    @players = player_1
+  end
+
+  def player_1
+    @players
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,11 @@
 require 'game'
 
 describe Game do
+
+  subject(:game) { described_class.new(player_1) }
+  let(:player_1) { Player.new('Richard') }
+  
+  it 'retrieves Player 1' do
+    expect(subject.players).to eq(player_1)
+  end
 end
