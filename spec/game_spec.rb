ea5ed752852830c16df8ae1d7eb8b2aa251c7523
PatrickBarsch <PatrickGB@gmx.de>
2021-11-01 08:56:24 +0100

    Added test to check that win by player 2 is captured correctly

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -21,5 +21,18 @@ describe Game do
       end
       expect(actual_wins).to eq expected_wins
     end
+    it 'determines a win by player two correclty' do
+      winning_weapons = [:Rock, :Paper, :Scissors]
+      loosing_weapons = [:Scissors, :Rock, :Paper]
+      expected_wins = [:player2, :player2, :player2]
+      actual_wins = []
+      winning_weapons.each_with_index do |weapon, i|
+        player1 = double(name: :player1, weapon: loosing_weapons[i])
+        player2 = double(name: :player2, weapon: weapon)
+        game = Game.new(player1, player2)
+        actual_wins << game.winner
+      end
+      expect(actual_wins).to eq expected_wins
+    end
   end
 end
