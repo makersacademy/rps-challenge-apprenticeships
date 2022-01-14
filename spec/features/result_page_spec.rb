341375fb16347c90a81a1825558559e0b27f4011
Orla <orladunlop2@gmail.com>
2022-01-14 14:30:01 +0000

    Results page now has a replay button which sends back to play page

--- spec/features/result_page_spec.rb
+++ spec/features/result_page_spec.rb
@@ -4,6 +4,13 @@ feature 'displays the result of the match' do
     choose_rock
     expect(page).to have_content "Result"
   end
+
+  scenario 'user can click the "play again" button to return to start an new round' do
+    enter_name_and_start
+    choose_rock
+    find('#replay').click
+    expect(page).to have_current_path("/play")
+  end
 end
 
 # given (:other_user) { User.make(email: 'other@example.com', password: 'rous')}
