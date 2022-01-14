3ed3f6140588cbe578da3ced90c1127bb20ee822
Jackie Benn <jackiebenn84@hotmail.com>
2022-01-14 10:59:33 +0000

    added form form rock, paper, scissors

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -5,4 +5,12 @@ feature 'play' do
     click_button 'submit'
     expect(page).to have_content 'Hi jackie'
   end
+
+  scenario 'player can choose an option from the select box' do
+    visit '/'
+    fill_in 'name', with: 'jackie'
+    click_button 'submit'
+    select('Rock', from: 'options')
+    expect(page).to have_content 'Rock'
+  end
 end
