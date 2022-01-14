b8ab406bce6565f98fdce4409e124d09fe427fe1
Aadam Ali <ali.aadam@outlook.com>
2022-01-14 13:22:25 +0000

    updated file to order by input rather than result

--- spec/unit/game_spec.rb
+++ spec/unit/game_spec.rb
@@ -16,45 +16,46 @@ describe Game do
   end
 
   describe '#result' do
-    context 'User wins' do
+    context 'User enters rock' do
       it 'returns \'You Won\' when user choice is rock and computer choice is scissors' do
         expect(Game.new('rock', 'scissors').result).to eq 'You Won'
-      end
-
-      it 'returns \'You Won\' when user choice is paper and computer choice is rock' do
-        expect(Game.new('paper', 'rock').result).to eq 'You Won'
-      end
-
-      it 'returns \'You Won\' when user choice is scissors and computer choice is paper' do
-        expect(Game.new('scissors', 'paper').result).to eq 'You Won'
-      end
-    end
-
-    context 'User loses' do
-      it 'returns \'You Lost\' when user choice is scissors and computer choice is rock' do
-        expect(Game.new('scissors', 'rock').result).to eq 'You Lost'
       end
 
       it 'returns \'You Lost\' when user choice is rock and rock choice is paper' do
         expect(Game.new('rock', 'paper').result).to eq 'You Lost'
       end
 
-      it 'returns \'You Lost\' when user choice is paper and computer choice is scissors' do
-        expect(Game.new('paper', 'scissors').result).to eq 'You Lost'
+      it 'returns \'You Drew\' when both choices are rock' do
+        expect(Game.new('rock', 'rock').result).to eq 'You Drew'
       end
     end
 
-    context 'Draw' do
-      it 'returns \'You Drew\' when both choices are rock' do
-        expect(Game.new('rock', 'rock').result).to eq 'You Drew'
+    context 'User enters paper' do
+      it 'returns \'You Won\' when user choice is paper and computer choice is rock' do
+        expect(Game.new('paper', 'rock').result).to eq 'You Won'
+      end  
+
+      it 'returns \'You Lost\' when user choice is paper and computer choice is scissors' do
+        expect(Game.new('paper', 'scissors').result).to eq 'You Lost'
       end
 
       it 'returns \'You Drew\' when both choices are paper' do
         expect(Game.new('paper', 'paper').result).to eq 'You Drew'
       end
+    end
+
+    context 'User enters scissors' do
+      it 'returns \'You Won\' when user choice is scissors and computer choice is paper' do
+        expect(Game.new('scissors', 'paper').result).to eq 'You Won'
+      end
+
+      it 'returns \'You Lost\' when user choice is scissors and computer choice is rock' do
+        expect(Game.new('scissors', 'rock').result).to eq 'You Lost'
+      end
 
       it 'returns \'You Drew\' when both choices are scissors' do
         expect(Game.new('scissors', 'scissors').result).to eq 'You Drew'
-      end    end
+      end    
+    end
   end
 end
