a2239def5c40eafbc74b4b1d83985a17b4d9d393
Michael Zwickler <zwickler.michael@googlemail.com>
2022-01-14 11:03:47 +0100

    feature test for multi-player sign in added

--- app.rb
+++ app.rb
@@ -9,12 +9,16 @@ class RockPaperScissors < Sinatra::Base
 
   post '/sign_in' do
     session[:player_1] = params[:player_1]
-    session[:player_2] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    session[:time] = Time.now
     redirect '/play'
   end
 
   get '/play' do
-    "#{session[:player_1]} vs. Computer"
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    @player_2 = 'Computer' if @player_2.empty?
+    erb(:play)
   end
 
   run! if app_file == $0
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,10 +1,21 @@
 feature 'sign in with your name' do
-  
-  scenario 'will set up a game showing the users and the opponents name' do
-    visit('/')
-    fill_in('Player 1', with: 'Freddy')
-    click_button('Start')
-    expect(page).to have_content('Freddy vs. Computer')
+
+  context 'when single player mode' do
+    scenario 'will set up a game showing the username vs computer' do
+      visit('/')
+      fill_in('Player 1', with: 'Freddy')
+      click_button('Start')
+      expect(page).to have_content('Freddy vs. Computer')
+    end
+  end
+  context 'when multi player mode' do
+    scenario 'will set up a game showing both usernames' do
+      visit('/')
+      fill_in('Player 1', with: 'Freddy')
+      fill_in('Player 2', with: 'Jason')
+      click_button('Start')
+      expect(page).to have_content('Freddy vs. Jason')
+    end
   end
 
 end
