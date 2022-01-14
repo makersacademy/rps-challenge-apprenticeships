f7d15fa351de0eabb000b9082388e5a154b3504e
Marcus <marcusventin@gmail.com>
2022-01-14 15:52:13 +0000

    Updated tests to account for lizards and spocks

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,6 +7,9 @@ describe Game do
   let (:rock) { double :player, name: "rock", move: "Rock" }
   let (:paper) { double :player, name: "paper", move: "Paper" }
   let (:scissors) { double :player, name: "scissors", move: "Scissors" }
+  let (:lizard) { double :player, name: "lizard", move: "Lizard" }
+  let (:spock) { double :player, name: "Spock", move: "Spock" }
+
 
   subject { described_class.new(player_one, player_two, nil) }
 
@@ -28,9 +31,17 @@ describe Game do
       it "p1 = Scissors and p2 = Paper" do
         expect(subject.outcome(scissors, paper)).to eq scissors
       end
+
+      it "p1 = Lizard and p2 = Paper" do
+        expect(subject.outcome(lizard, paper)).to eq lizard
+      end
+
+      it "p1 = Spock and p2 = Rock" do
+        expect(subject.outcome(spock, rock)).to eq spock
+      end
     end
 
-    context "returns CPU wins when" do
+    context "returns player_2 when" do
       it "p1 = Rock and p2 = Paper" do
         expect(subject.outcome(rock, paper)).to eq paper
       end
@@ -41,6 +52,14 @@ describe Game do
 
       it "p1 = Scissors and p2 = Rock" do
         expect(subject.outcome(scissors, rock)).to eq rock
+      end
+
+      it "p1 = Lizard and p2 = Rock" do
+        expect(subject.outcome(lizard, rock)).to eq rock
+      end
+
+      it "p1 = Spock and p2 = Paper" do
+        expect(subject.outcome(spock, paper)).to eq paper
       end
     end
 
@@ -55,6 +74,14 @@ describe Game do
 
       it "p1 = Scissors and p2 = Scissors" do
         expect(subject.outcome(scissors, scissors)).to eq "Draw"
+      end
+
+      it "p1 = Lizard and p2 = Lizard" do
+        expect(subject.outcome(lizard, lizard)).to eq "Draw"
+      end
+
+      it "p1 = Spock and p2 = Spock" do
+        expect(subject.outcome(spock, spock)).to eq "Draw"
       end
     end
   end
