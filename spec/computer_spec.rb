238be19f255750e830afa77c4d70a2e9693a9235
JALWilliams <just.williams04@gmail.com>
2022-01-14 11:07:03 +0000

    Additional files missed in previous safe

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/computer.rb'
 describe Computer do
   subject(:comp) { described_class.new }
 
-  it 'cam choose a random option of Rock, Paper, Scissors' do
+  it 'can choose a random option of Rock, Paper, Scissors' do
     allow(comp).to receive(:rps).and_return("Rock")
     expect(comp.rps).to eq("Rock")
   end
--- spec/features/play_again_spec.rb
+++ spec/features/play_again_spec.rb
@@ -3,7 +3,7 @@ feature 'play again' do
   register_and_play
   click_button 'Rock'
   click_button 'Play again'
-  expect(page).to have_content('your move!')
+  expect(page).to have_content('your move!")
   end
 
 end
