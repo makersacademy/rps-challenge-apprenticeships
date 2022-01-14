ae60a66294e08ef4809882ce82176fe8087058c3
Michael Zwickler <zwickler.michael@googlemail.com>
2022-01-14 13:19:55 +0100

    test refactoring with let

--- spec/unit/rps_game_spec.rb
+++ spec/unit/rps_game_spec.rb
@@ -1,20 +1,20 @@
 RSpec.describe RPSGame do
 
+  let (:multiplayer_game) { described_class.new("Freddy", "Jason") }
+  let (:singleplayer_game) { described_class.new("Freddy") }
+
   describe '#initialize' do
     context 'when single player' do
       it 'stores the players name' do
-        subject = RPSGame.new("Freddy")
-        expect(subject.player_1).to eq("Freddy")
+        expect(singleplayer_game.player_1).to eq("Freddy")
       end
       it 'stores Computer in player 2' do
-        subject = RPSGame.new("Freddy")
-        expect(subject.player_2).to eq("Computer")
+        expect(singleplayer_game.player_2).to eq("Computer")
       end
     end
     context 'when multiplayer player' do
       it 'stores the second players name' do
-        subject = RPSGame.new("Freddy", "Jason")
-        expect(subject.player_2).to eq("Jason")
+        expect(multiplayer_game.player_2).to eq("Jason")
       end
     end
   end
@@ -22,20 +22,16 @@ RSpec.describe RPSGame do
   describe '#play' do
     context 'when multiplayer' do
       it 'Rock beats scissors' do
-        subject = RPSGame.new("Freddy", "Jason")
-        expect(subject.play('Rock', 'Scissors')).to eq('Freddy')
+        expect(multiplayer_game.play('Rock', 'Scissors')).to eq('Freddy')
       end
       it 'Paper beats rock' do 
-        subject = RPSGame.new("Freddy", "Jason")
-        expect(subject.play('Rock', 'Paper')).to eq('Jason')
+        expect(multiplayer_game.play('Rock', 'Paper')).to eq('Jason')
       end
       it 'Scissors beats papers' do
-        subject = RPSGame.new("Freddy", "Jason")
-        expect(subject.play('Scissors', 'Paper')).to eq('Freddy')
+        expect(multiplayer_game.play('Scissors', 'Paper')).to eq('Freddy')
       end
       it 'Scissors draws scissors' do
-        subject = RPSGame.new("Freddy", "Jason")
-        expect(subject.play('Scissors', 'Scissors')).to eq('Draw')
+        expect(multiplayer_game.play('Scissors', 'Scissors')).to eq('Draw')
       end
     end
 
