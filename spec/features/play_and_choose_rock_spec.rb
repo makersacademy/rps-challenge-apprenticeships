a832f1e8433b2d3c370788ee51adc357dbff6711
Adam Hindmarsh <adam_hindmarsh@hotmail.com>
2022-01-19 11:32:33 +0000

    css updated and buttons added rather than form

--- spec/features/play_and_choose_rock_spec.rb
+++ spec/features/play_and_choose_rock_spec.rb
@@ -1,8 +1,7 @@
 feature 'login and choose the option rock' do 
   scenario ' login with Adam and choose rock' do 
     sign_in
-    fill_in :choice , with:"rock"
-    click_button "Submit"
+    click_button "rock"
     expect(page).to have_content "Adam chose rock"
   end
 end
