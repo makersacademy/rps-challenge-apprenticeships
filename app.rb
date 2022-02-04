d63d70ce36024e32461521d68249ae0f4062e79c
benfielding74 <billy.fadden@live.com>
2022-02-04 12:12:04 +0000

    Second feature test, testing GET/POST routes, implemented and passed

--- app.rb
+++ app.rb
@@ -1,8 +1,26 @@
+require 'sinatra'
 require 'sinatra/base'
+require 'sinatra/reloader' if development?
+
 class RockPaperScissors < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  enable :sessions
 
   get '/' do
     erb :index
+  end
+
+  post '/names' do
+    session[:player_name] = params[:player_name]
+    redirect to '/play'
+  end
+
+  get '/play' do
+    @player_name = session[:player_name]
+    erb :play
   end
 
   get '/test' do
--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -2,5 +2,14 @@ feature 'User visits app page' do
   scenario 'there is a form to enter name and start game' do
     visit ('/')
     find_field('player_name').value
+    find_button('Start Game').click
   end
+
+  scenario 'user enters name and clicks to play' do
+    visit ('/')
+    fill_in 'player_name', with: 'Yossarian'
+    click_button 'Start Game'
+    expect(page).to have_content("Choose your weapon Yossarian")
+  end
+
 end
