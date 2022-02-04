f322a7386354adec79e8c85f957b75d96c2c649c
Eden <elsw@hotmail.co.uk>
2022-02-04 12:40:28 +0000

    user can fill in name and have it displayed back.

--- app.rb
+++ app.rb
@@ -1,5 +1,32 @@
 require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
+  
+  get '/' do 
+    "button linking to players page to go here"
+    '<form method="get" action="/playersnames">
+      <button type="submit"> Lets Game</button>
+    </form>'
+    
+  end
+
+
+  get '/playersnames' do
+  
+    erb :index
+  end
+
+  post '/players' do
+    p params
+    @player1 = params[:player1]
+    @player2 = params[:player2]
+    erb(:new)
+  end
+
+
+  
+  
+  
+  
   get '/test' do
     'test page'
   end
---
+++ spec/features/players_page_spec.rb
@@ -0,0 +1,13 @@
+feature 'Players enter names' do 
+  scenario 'user can fill in name on form' do 
+    visit('/playersnames')
+    fill_in :player1, with: "eden"
+    fill_in :player2, with: "lindy"
+    click_button 'Battle'
+    expect(page).to have_content 'eden vs. lindy'
+
+
+
+end
+
+end
