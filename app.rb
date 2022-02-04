63829684849468b2739a8dde5bdc3779cecd5aea
Lauren Gray <laurenbethgrayx@gmail.com>
2022-02-04 11:38:54 +0000

    First Commit - User add name - test passes

--- app.rb
+++ app.rb
@@ -1,8 +1,27 @@
 require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
-  get '/test' do
-    'test page'
+
+  get '/' do
+    erb :index
   end
+
+  post '/name' do
+    @player_name = params[:player_name]
+    erb :play
+  end
+
+
+
+
+
+
+
+
+
+
+  # get '/test' do
+  #   'test page'
+  # end
 
   run! if app_file == $0
 end
