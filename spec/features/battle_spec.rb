30b1ec147ae91df09317a61ff78868f409cfe858
Will Diable <wtdiable@icloud.com>
2022-02-04 17:36:14 +0000

    receive attack choice confirmation refactored

--- spec/features/battle_spec.rb
+++ spec/features/battle_spec.rb
@@ -1,6 +1,6 @@
 feature 'battle' do
 
-  scenario 'choose R,P or S via button and receive confirmation' do
+  scenario 'choose attack via button and receive confirmation' do
     sign_in
     click_button 'Rock'
     expect(page).to have_content 'Wilbur chose Rock'
