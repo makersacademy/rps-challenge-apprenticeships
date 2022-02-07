227c8089cea6f2a6d823e85b6fc30bea0176ac0f
Jonah Massam <Jonah.Massam10@yahoo.co.uk>
2022-02-07 02:15:04 +0000

    Input check and replay

--- app.rb
+++ app.rb
@@ -31,12 +31,14 @@ class RockPaperScissors < Sinatra::Base
 
   get "/game" do
     @player_name = session[:player_name]
+    @error = session[:error]
     erb :game
   end
 
   post "/set_game_choice" do
     session[:player_choice] = params[:player_choice].downcase
     session[:computer_choice] = ["rock", "paper", "scissors"].sample
+    session[:error] = false
     redirect :result
   end
 
@@ -44,21 +46,28 @@ class RockPaperScissors < Sinatra::Base
     @player_choice = session[:player_choice]
     @computer_choice = session[:computer_choice]
     @result = check_result(@player_choice, @computer_choice)
+    if @result == "error"
+      session[:error] = true
+      redirect :game
+    end
     erb :result
+  end
+
+
+  def check_result(player_choice, computer_choice)
+    return "error" unless ["rock", "paper", "scissors"].include? player_choice
+    return "draw" if player_choice == computer_choice
+    case player_choice
+    when "rock"
+      computer_choice == "paper" ? "lose" : "win"
+    when "paper"
+      computer_choice == "scissors" ? "lose" : "win"
+    when "scissors"
+      computer_choice == "rock" ? "lose" : "win"
+    end
   end
 
   run! if app_file == $0
 end
 
 
-def check_result(player_choice, computer_choice)
-  return "draw" if player_choice == computer_choice
-  case player_choice
-  when "rock"
-    computer_choice == "paper" ? "lose" : "win"
-  when "paper"
-    computer_choice == "scissors" ? "lose" : "win"
-  when "scissors"
-    computer_choice == "rock" ? "lose" : "win"
-  end
-end
