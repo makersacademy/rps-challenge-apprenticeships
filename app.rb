480dd19185638258d964427d6c0d83151a27df9e
Siddiq Rahman <sidq@hotmail.co.uk>
2022-02-07 01:35:27 +0000

    First user story completed

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
   get '/test' do
-    'test page'
+    p params
+    @name = params[:name]
+    erb(:index)
   end
+
+  
 
   run! if app_file == $0
 end
+
