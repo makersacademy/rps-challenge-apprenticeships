43bc8d8a727718fd09de77f08c731bf8a52cb9d5
Jon Clarke <jonclarke1984@googlemail.com>
2022-03-25 16:56:39 +0000

    written test for starting the pvp game'

--- spec/features/choose_ai_or_pvp_spec.rb
+++ spec/features/choose_ai_or_pvp_spec.rb
@@ -6,4 +6,14 @@ feature 'choose to play vs AI or another player' do
     click_button 'Submit'
     expect(page.html).to include("<h1>Homer, make your choice</h1>")
   end
+
+  scenario 'the player chooses to play vs another player' do
+    visit('/')
+    click_button 'Player vs Player'
+    fill_in 'player_one', with: 'Homer'
+    fill_in 'player_one', with: 'Marge'
+    click_button 'Submit'
+    click_button 'rock'
+    expect(page.html).to include("<h1>Marge, make your choice</h1>")
+  end
 end
