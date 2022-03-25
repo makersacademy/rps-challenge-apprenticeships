f55329bd4704d9b73f1a947503bea430e79f9659
Peter Hattyar <peter.hattyar@gmail.com>
2022-03-25 11:49:05 +0000

    created main landing page with form

--- spec/features/test_page_spec.rb
+++ spec/features/test_page_spec.rb
@@ -4,3 +4,10 @@ feature 'test page' do
     expect(page).to have_content('test page')
   end
 end
+
+feature 'main page' do
+  scenario 'visit main page' do
+    visit '/'
+    expect(page).to have_content('Welcome to RPS! Please enter your name')
+  end
+end
