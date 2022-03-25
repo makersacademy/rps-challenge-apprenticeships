2a89840fe2366ed7f334a294c9ccdb04c34a0c85
Bradley Deakin <bradleydeakin2@gmail.com>
2022-03-25 12:52:43 +0000

    added routes to display enter name form and choice selector form

--- app.rb
+++ app.rb
@@ -1,8 +1,27 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
+
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
+
+  get '/' do
+    erb :index
+  end
+
+  post '/name' do
+    session[:player_name] = params[:player_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_name = session[:player_name]
+    erb :move
+  end
+
   get '/test' do
     'test page'
   end
 
+  
   run! if app_file == $0
 end
