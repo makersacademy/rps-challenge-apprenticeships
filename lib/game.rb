d71235f8769c2c31f9c3424128498878ff704093
Daniel Taplin <98479830+dantaplin992@users.noreply.github.com>
2022-03-25 15:58:56 +0000

    better game

--- lib/game.rb
+++ lib/game.rb
@@ -18,22 +18,18 @@ class Game
   end
 
   def result
-    get_result(@my_answer, opponent_answer)
+    get_result(opponent_answer)
   end
 
   private
 
-  def get_result(my_answer, opponent_answer)
-    if my_answer == opponent_answer
+  def get_result(opponent_answer)
+    if @my_answer == opponent_answer
       "Draw"
-    elsif @answers[@answers.index(my_answer) - 1] == opponent_answer
+    elsif @answers[@answers.index(@my_answer) - 1] == opponent_answer
       "Win"
     else
       "Lose"
     end
   end
 end
-
-# game = Game.new("Rock")
-# p game.opponent_answer
-# binding.irb
