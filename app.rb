522d8433f11887cecd3e32816166654377dc3c7a
Adam Deakin <adeakin89@gmail.com>
2022-03-25 11:44:05 +0000

    Added enter name feature

--- app.rb
+++ app.rb
@@ -1,7 +1,23 @@
 require 'sinatra/base'
+require './lib/player.rb'
+
 class RockPaperScissors < Sinatra::Base
   get '/test' do
     'test page'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    $player = Player.new(params[:player_name])
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player = $player
+    erb :play
   end
 
   run! if app_file == $0
