5ba5b0dc2be5de0b4986a29d236f78d23f9c8d44
Khushnood Asif <khushnood99@gmail.com>
2022-03-28 14:13:01 +0100

    fix issue with computer.rb

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,6 +1,6 @@
 class Computer
   def move
-    random_num = rand(1..2)
+    random_num = rand(1..3)
     @move = 'Rock' if random_num  == 1
     @move = 'Paper' if random_num == 2
     @move = 'Scissor' if random_num == 3
