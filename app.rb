0db426bf15fd252f030705204d071f173316fdf9
sabih <sabihsaleh@yahoo.com>
2022-07-08 18:16:52 +0100

    Files Updated

--- app.rb
+++ app.rb
@@ -10,24 +10,9 @@ class RockPaperScissors < Sinatra::Base
   end
   post '/action_page' do
      p params
-  #   session[:player] = Player.new(params[:player_name])
-  #   redirect '/select-action'
+     @name = player
   end
 
-  # get '/select-action' do
-  #   @player_name = session[:player].name
-  #   erb :select_action
-  # end
-
-  # post '/record-action' do
-  #   session[:player].choose_action(params[:action])
-  #   redirect '/display-action'
-  # end
-
-  # get '/display-action' do
-  #   @player_action = session[:player].action
-  #   erb :display_action
-  # end
   
 
     run! if app_file == $0
