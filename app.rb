9dcf5de8d50a743662fd21ee1e886c95671e01fb
KMTARBUCK <kmtarbuck@gmail.com>
2022-07-08 14:06:20 +0100

    final check for now

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
   get '/test' do
-    'testing testing again'
+    'testing testing one last time!'
   end
 
   run! if app_file == $0
