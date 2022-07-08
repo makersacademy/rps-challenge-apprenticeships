c8743a2ce2f31318845f6b5c5f0fb083e00bb031
Marlon-Buckley <m4rlz@live.co.uk>
2022-07-08 14:17:11 +0100

    adding webpage structure

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 ruby '3.0.2'
 
 gem 'sinatra'
+gem 'puma'
+gem 'webdrivers', '~> 5.0', require: false
 
 group :test do
   gem 'capybara'
--- app.rb
+++ app.rb
@@ -7,8 +7,21 @@ class RockPaperScissors < Sinatra::Base
   end
 
   get '/' do
-    "hello world"
+    erb(:index)
   end
+
+  post '/' do
+    @player_name = params["name"]
+    @player_move = params["move"]
+    p params
+    p @player_move
+    erb(:index)
+  end
+
+  # get 'the_game.rb' do
+  #   erb(:the_game)
+  # end
+
 
   run! if app_file == $0
 end
