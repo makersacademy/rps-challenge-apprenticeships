414bbb3f7332d2738c3afa556c13c94ef8887e97
erb16 <erbayliss16@gmail.com>
2022-07-08 13:36:11 +0100

    player able to submit name

--- app.rb
+++ app.rb
@@ -1,8 +1,33 @@
 require 'sinatra/base'
+# require "sinatra/reloader" if development?
 class RockPaperScissors < Sinatra::Base
-  get '/test' do
-    'test page'
+
+  enable :sessions
+  
+  # get '/test' do
+  #   'test page'
+  # end
+
+  get '/' do
+    # session["player_name"] = params["player_name"]
+    erb(:index)
+    # redirect '/submit-name'
+  end
+
+  post '/submit-name' do
+
+    session["player_name"] = params["player_name"]
+    @player_name = session["player_name"]
+    # erb (:submitted-name)
+    erb(:results)
+  end
+
+  get '/results' do
+    @player_name = session["player_name"]
+
+    erb(:results)
   end
 
   run! if app_file == $0
+
 end
---
+++ spec/feature_spec.rb
@@ -0,0 +1,6 @@
+describe '#name' do
+  it 'returns the name' do
+    expect(Bob.name).to eq 'Bob'
+  end
+end
+
