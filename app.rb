bcf4397f1d37cc251e778910da90b8a4aedbded3
KevinByrne <kevbee79@gmail.com>
2022-07-08 09:05:37 +0100

    index route added to app.rb, views directory added, index.erb file added

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
   get '/test' do
     'test page'
   end
+
 
   run! if app_file == $0
 end
