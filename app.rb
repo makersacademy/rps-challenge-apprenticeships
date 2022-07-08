dd30e90a829f03556923d137988986884b5aa050
HanzAkor <95639001+HanzAkor@users.noreply.github.com>
2022-07-08 14:28:39 +0100

    Created /names page

--- app.rb
+++ app.rb
@@ -1,8 +1,27 @@
 require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
-  get '/test' do
-    'home page'
+
+  enable :sessions
+
+  # get '/test' do
+  #   'home page'
+  # end
+
+  get '/' do
+   erb :index
   end
+
+	post '/names' do
+		session[:player_name] = params[:player_name]
+  	redirect '/play'
+	end
+
+  get '/play' do
+    @player_name = session[:player_name] 
+    erb :play
+  end
+
+
 
   run! if app_file == $0
 end
--- spec/features/test_page_spec.rb
+++ spec/features/test_page_spec.rb
@@ -1,6 +1,18 @@
-feature 'test page' do
-  scenario 'visit test page' do
-    visit '/test'
-    expect(page).to have_content('test page')
+# feature 'test page' do
+#   scenario 'visit test page' do
+#     visit '/test'
+#     expect(page).to have_content('home page')
+#   end
+# end
+
+feature 'Enter name' do
+  scenario 'submitting name' do
+    # sign_in_and_play
+    visit('/')
+    fill_in :player_name, with: 'Hannah'
+    click_button 'Submit'
+    # save_and_open_page # will save the web page and open the browser to display it
+
+    expect(page).to have_content 'Hannah vs. Computer'
   end
 end
