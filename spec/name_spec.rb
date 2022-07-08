79162ccdab92bf10eb647ae07e7e9a8e9ec62efb
sabih <sabihsaleh@yahoo.com>
2022-07-08 15:32:42 +0100

    Player Name Displayed

--- spec/name_spec.rb
+++ spec/name_spec.rb
@@ -1,9 +1,9 @@
 
-# feature 'Player name' do
-#     it 'dispaly player name' do
-#         visit '/'
-#         fill_in 'name', with: 'Sabih'
-#         click_button 'Submit'
-#         expect(page).to have_content('Sabih')
-#     end
-# end
+feature 'Player name' do
+    it 'dispaly player name' do
+        visit '/'
+        fill_in 'name', with: 'Sabih'
+        click_button 'Submit'
+        expect(page).to have_content('Sabih')
+    end
+end
