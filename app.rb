ac6af7eade916dce59f363c1b01c56e7c8cdb193
Amy-O <amyalice88@gmail.com>
2022-07-08 16:09:55 +0100

    return name in central and in red

--- app.rb
+++ app.rb
@@ -1,13 +1,18 @@
 require 'sinatra/base'
+
 class RockPaperScissors < Sinatra::Base
-  get '/rockpaperscissors' do
+  
+  get '/nameform' do
     erb :name_form
+  end
+  
+  post '/yourname' do
+    p params
+    @name = params[:name]
+    erb :index
   end
 
   run! if app_file == $0
+
 end
 
-post '/yourname' do
-  @name = params[:name]
-  erb :index
-end
