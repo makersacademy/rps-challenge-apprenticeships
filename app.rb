766de1382296653033057f904d5a0d27dd04d2cd
sabih <sabihsaleh@yahoo.com>
2022-07-08 22:40:36 +0100

    As far as Weapon choice

--- app.rb
+++ app.rb
@@ -9,11 +9,22 @@ class RockPaperScissors < Sinatra::Base
     erb :index
   end
   post '/action_page' do
-     p params
-     @name = player
+     session[:name] = params[:name]
+     redirect '/play'
   end
-
-  
-
+  get '/play' do
+    @player_name = session[:name]
+    erb :play
+  end
+  post '/record-action' do
+    session[:action] = params[:action]
+    redirect '/result'
+  end
+  get '/result' do
+    @selection = session[:action]
+    @opponent = [:Rock, :Paper, :Scissors].sample
+    erb :result
+  end
+    
     run! if app_file == $0
 end
