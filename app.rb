6ad9ddf54f255d7051fb5cf3e297346d5375c1c7
Marlon-Buckley <m4rlz@live.co.uk>
2022-07-10 11:42:00 +0100

    removing files

--- app.rb
+++ app.rb
@@ -7,17 +7,18 @@ class RockPaperScissors < Sinatra::Base
   end
 
   post '/player-greet' do
-    player = params["player_name"]
+    @player = params["player_name"] #captures player name entered on index page form
+    p @player
     redirect '/game'
   end
 
-  post '/' do
-    @player_name = params["name"]
-    @player_move = params["move"]
-    p params
-    p @player_move
-    erb(:index)
-  end
+  # post '/' do
+  #   @player_name = params["name"]
+  #   @player_move = params["move"]
+  #   p params
+  #   p @player_move
+  #   erb(:index)
+  # end
 
   run! if app_file == $0
 end
