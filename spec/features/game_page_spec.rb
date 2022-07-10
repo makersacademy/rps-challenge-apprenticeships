19a18876c8fc3644e3da0985041ccc25bcacc8d3
Marlon-Buckley <m4rlz@live.co.uk>
2022-07-10 12:43:30 +0100

    adding test for rock, paper, scissors buttons appearing

--- spec/features/game_page_spec.rb
+++ spec/features/game_page_spec.rb
@@ -2,6 +2,11 @@ feature 'game page' do
   scenario 'visit game page greet user with their name' do
     fill_name_and_play
     expect(page).to have_content('Hello Marlon, welcome to the game! Please choose your move.')
+  end
+
+  scenario 'game page should have rock, paper, scissors buttons' do
+    fill_name_and_play
+    expect(page).to have_selector(:link_or_button, 'Rock')
     save_and_open_page
   end
 end
