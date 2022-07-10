246f1e98fe64844b52231b48e9dc5ecd58d9ecdd
Marlon-Buckley <m4rlz@live.co.uk>
2022-07-10 12:45:16 +0100

    updating test to include Paper & Scissors buttons

--- spec/features/game_page_spec.rb
+++ spec/features/game_page_spec.rb
@@ -7,6 +7,8 @@ feature 'game page' do
   scenario 'game page should have rock, paper, scissors buttons' do
     fill_name_and_play
     expect(page).to have_selector(:link_or_button, 'Rock')
+    expect(page).to have_selector(:link_or_button, 'Paper')
+    expect(page).to have_selector(:link_or_button, 'Scissors')
     save_and_open_page
   end
 end
